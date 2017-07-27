217448689998a2224c0dc02e9da7059e9494fbf4
malawi-mike <mike@villagex.org>
2017-07-27 10:26:47 -0400

    Commit message

--- PrivacyPolicy.php
+++ PrivacyPolicy.php
@@ -1,1 +1,42 @@
-<?php
+<?php include('header.inc'); ?>
+
+<div class="container">
+	<h3 style="text-align:center">Privacy Policy</h3>
+	
+		<p>This privacy policy covers how Friends of Village X Inc (dba Village X Org) treats information it collects and receives about its users. Anyone who visits www.villagex.org or uses or accesses any of Village X's applications shall by so doing be deemed to have agreed to the terms of this privacy policy and such collection, use and other processing of data as set forth below.</p>
+		<p>All capitalized words and expressions used and not otherwise defined herein have the meanings assigned to them in Village X's terms of service (available at www.villagex.org and hereinafter referred to as "Terms of Service"), with the exception of the term "Customer" which, for the purposes of this privacy policy, shall also include anyone who visits www.villagex.org or uses or accesses, or attempts to use or access, any of Village X's applications. The term "Supplier" refers to Village X Org, a nonprofit in Washington, DC.</p>
+		<p>Nothing herein shall in any manner limit, exclude or prejudice, or be construed as a waiver of, any right or freedom that Supplier may have under applicable law.</p>
+	
+	<h6><b>Data Collection</b></h6>
+		<p>Customer acknowledges that Supplier may collect information about Customer and Third-party Users: (i) upon one's opening of a User Account, creation or modification of a user profile, use of a particular Workspace (the information collected includes such Customer Details and/or other information as entered or generated in the sign-up widow, on Customer's personal profile page or elsewhere in the application); (ii) when they visit Supplier's website (session information, browsing history at Supplier's website, IP address, certain software and hardware attributes) or fill in forms via the Service (the information thus provided); (iii) when they access or use the Service (the location, manner, means and duration of such use or access); and (iv) when otherwise knowingly made available to Supplier (the information the data subjects provide). When visiting Supplier's website, "cookies" are likely to be stored within the visitor's device.</p>
+	
+	<h6><b>Data Processing</b></h6>
+		<p>Customer agrees and warrants to Supplier that Customer and Third-party Users agree:</p>
+			<ol>
+				<li>To Supplier's processing of Customer Details and such other information as referred to in section 4 (collectively, "User Data") for the purposes of (a) providing the Service, (b) improving or otherwise modifying the Service and notifying Customer thereof, (c) customizing the content and/or layout of Supplier's website for the particular visitor, (d) replying to Customer's communications and contacting Customer, (e) performing Supplier's obligations under the Agreement, (f) exercising and enforcing Supplier's rights and freedoms, (g) statistics and other analyses;</li>
+				<li>That User Data may be processed in the country of their domicile as well as outside it (including, without limitation, the United States of America, the Swiss Confederation and the member states of the European Economic Area);</li>
+				<li>That Supplier will not disclose Customer Details to any third party besides the members of its corporate group, except that it will disclose (a) user profile information of Customers to other application users, such as campaigns supported, projects supported, and donations made (excluding the dollar amount), unless a Customer has made that information private in the application settings, in which case Supplier will disclose only basic profile information provided by Customer, such as username, date of birth, and city of residence, (b) Customer Details required by law, or (c) Customer Details necessary in order to perform Supplier's obligations under the Agreement or to exercise its legal rights or freedoms or defend against claims or other process.</li>
+			</ol>
+		
+		<p>Supplier represents that it has implemented and will continue to employ commercially reasonable measures to ensure that Customer Details are processed securely and in compliance with applicable law.
+		Supplier has no obligation to monitor or access customer accounts, but may do so in cases where such action is reasonably justified (e.g., in order to prevent illegal or harmful activity or to provide customer support or to perform a legal obligation it otherwise cannot perform).
+		Supplier shall not be considered a controller or processor (neither chief, responsible, authorized nor any other processor) of any Workspace Data and has no obligation whatsoever in connection with any Workspace Data or the processing thereof. Pursuant to the Terms of Service, Workspace Data is deemed to be processed by registered Customers and any issue concerning such data or its processing should be taken up with the Customer designated as the administrator of the relevant Workspace.</p>
+			
+	<h6><b>Customer's Rights</b></h6>
+		<p>Upon Customer's request, Supplier will grant Customer access to, or, at Supplier's option, provide Customer with a statement of, all Customer Details that Supplier maintains about Customer, unless such information is already available to Customer via the Service or Supplier is prohibited by law from disclosing such records. Customer is entitled to correct, or have corrected, any of their Customer Details that are incorrect or misleading. Registered Customers can access and correct their Customer Details through the Service by visiting villagex.org. Customer acknowledges that any current or future Workspace Data is not maintained by Supplier and that this section does not apply to any information considered to be Workspace Data.</p>
+		<p>In all cases where Supplier is allowed to disclose Customer Details to third parties, it will, as far as possible, take reasonable measures to ensure that the person to whom disclosure is made grants Customer rights that are substantially similar to those set forth herein with respect to the processing of such Customer Details (including the right to be informed about the data maintained on Customer and the right to correct or have corrected incorrect or misleading data).</p>
+					
+	<h6><b>Notification</b></h6>
+		<p>Supplier may use their Customer Details to send them information about products, services, promotions and events which Supplier believes may be of interest to them. Customer's subscription to any such mailing program may be cancelled at will.</p>
+		<p>Supplier reserves the right to send registered Customers certain communications relating to the Service, such as service announcements and administrative messages, without offering Customer the opportunity to opt out of receiving them.</p>
+		<p>Supplier asks that all requests, inquiries, complaints and other communications that Customer wishes to address to Supplier with respect to this privacy policy or the processing of User Data be submitted via the feedback feature of Supplier's website or sent to the following email address: info@villagex.org.</p>
+	
+	<h6><b>Law and Jurisdiction</b></h6>
+		<p>This privacy policy and all matters relating to Supplier's data processing activities or concerning User Data shall be governed by the law applicable to the Terms of Service (as specified therein). All claims, disputes or other proceedings where Supplier is named as a party or is summoned or otherwise required to participate shall be subject exclusively to the jurisdiction specified in the said terms of service.</p>
+	
+	<h6><b>Change of Policy</b></h6>
+		<p>Supplier may amend this privacy policy at any time by posting a revised version on www.villagex.org. The revised version will take effect as of the time it is posted. In addition, if the revised version includes a significant change to the way that Customer Details may be treated, Supplier will provide registered Customers reasonable prior notice of such policy change by sending an email to the address Customer has associated with their User Account or by posting a prominent notice on the Service.</p>
+						    
+</div>
+
+<?php include('footer.inc'); ?>	
