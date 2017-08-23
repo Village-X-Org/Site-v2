159bf2a79958b3b9c4a37d2465a362647ffb2958
Jeff Depree <jdepree@gmail.com>
2017-08-23 13:50:34 -0600

    Adding stripe

--- index.php
+++ index.php
@@ -1,24 +1,38 @@
-<?php 
-require_once('utilities.php');
-include('header.inc');
- ?>
-
+<?php
+require_once ('utilities.php');
+include ('header.inc');
+?>
+<script>
+function addSubscription() {
+	
+}
+</script>
 <div id="index-banner" class="parallax-container"
 	style="background-color: rgba(0, 0, 0, 0.3); height: 500px">
-	<div class="section no-pad-bot valign-wrapper" style="height: 100%; width:100%;">
+	<div class="section no-pad-bot valign-wrapper"
+		style="height: 100%; width: 100%;">
 		<div class="row center">
-			<div style="padding: 5% 5% 5% 5%;"><h2
-				class="header col s12 white-text text-lighten-2 text-shadow: 2px 2px 7px #111111">Fund
-				Projects That Villages Choose</h2></div>
-		
-			<div style="padding: 5% 5% 5% 5%;"><br><br><h5 class="header center light text-shadow: 2px 2px 7px #111111">because everyone deserves
-			 a voice in development</h5></div>
-		
-			<div style="padding: 0% 5% 5% 5%;"><br><br><a href="project_tiles.php"
-					id="download-button"
+			<div style="padding: 5% 5% 5% 5%;">
+				<h2
+					class="header col s12 white-text text-lighten-2 text-shadow: 2px 2px 7px #111111">Fund
+					Projects That Villages Choose</h2>
+			</div>
+
+			<div style="padding: 5% 5% 5% 5%;">
+				<br>
+				<br>
+				<h5 class="header center light text-shadow: 2px 2px 7px #111111">because
+					everyone deserves a voice in development</h5>
+			</div>
+
+			<div style="padding: 0% 5% 5% 5%;">
+				<br>
+				<br>
+				<a href="project_tiles.php" id="download-button"
 					class="btn-large waves-effect waves-light light blue lighten-1">meet
-					the villages</a></div>
-					
+					the villages</a>
+			</div>
+
 			<div class="parallax">
 				<img src="images/header1.jpg">
 			</div>
@@ -28,7 +42,8 @@ include('header.inc');
 
 <div class="container">
 	<br>
-	<h4 class="header center light blue-text text-lighten-2">Help African villages disrupt extreme poverty</h4>
+	<h4 class="header center light blue-text text-lighten-2">Help African
+		villages disrupt extreme poverty</h4>
 
 	<!--   <h5 class="header center brown-text text-lighten-2">How It Works</h5> -->
 	<div class="section">
@@ -41,8 +56,9 @@ include('header.inc');
 					</h2>
 					<h5 class="center">Find a village</h5>
 
-					<p class="light center">Our partner villages are located in rural Africa, where extreme poverty is on the rise.
-					Our model and technology elevate their voices in international development.</p>
+					<p class="light center">Our partner villages are located in rural
+						Africa, where extreme poverty is on the rise. Our model and
+						technology elevate their voices in international development.</p>
 
 				</div>
 			</div>
@@ -54,9 +70,9 @@ include('header.inc');
 					</h2>
 					<h5 class="center">Fund a project</h5>
 
-					<p class="light center">Villages identify their biggest problems and propose
-						locally appropriate solutions. They also put skin in the game by
-						contributing cash, labor, and materials.</p>
+					<p class="light center">Villages identify their biggest problems
+						and propose locally appropriate solutions. They also put skin in
+						the game by contributing cash, labor, and materials.</p>
 				</div>
 			</div>
 
@@ -68,8 +84,8 @@ include('header.inc');
 					<h5 class="center">Track your impact</h5>
 
 					<p class="light center">Donors receive email updates with project
-						pictures and data showing exactly how each donation has helped. It's
-						our 100% transparency guarantee.</p>
+						pictures and data showing exactly how each donation has helped.
+						It's our 100% transparency guarantee.</p>
 				</div>
 			</div>
 		</div>
@@ -85,19 +101,19 @@ include('header.inc');
 
 		<!--   project section   -->
 		<div class="row">
-<?php 
-		$result = doQuery("SELECT project_id, project_name, picture_filename, project_summary, village_name, project_funded, project_budget FROM projects JOIN villages ON project_village_id=village_id JOIN pictures ON project_image_id=picture_id WHERE project_status = 'funding' LIMIT 3");
+<?php
+$result = doQuery("SELECT project_id, project_name, picture_filename, project_summary, village_name, project_funded, project_budget FROM projects JOIN villages ON project_village_id=village_id JOIN pictures ON project_image_id=picture_id WHERE project_status = 'funding' LIMIT 3");
 
-		while ($row = $result->fetch_assoc()) {
-		  $projectId = $row['project_id'];
-    	      $funded = $row['project_funded'];
-    	      $projectTotal = $row['project_budget'];
-    	      $fundedPercent = $funded / $projectTotal * 100;
-    	      $villageContribution = $projectTotal * .05;
-	      print "<div class='col s12 m4'>
+while ($row = $result->fetch_assoc()) {
+    $projectId = $row['project_id'];
+    $funded = $row['project_funded'];
+    $projectTotal = $row['project_budget'];
+    $fundedPercent = $funded / $projectTotal * 100;
+    $villageContribution = $projectTotal * .05;
+    print "<div class='col s12 m4'>
 			<div class='card sticky-action hoverable'>
 				<div class='card-image waves-effect waves-block waves-light'>
-					<img class='activator' src='".PICTURES_DIR."/{$row['picture_filename']}' onclick=\"document.location='project.php?id=$projectId';\">
+					<img class='activator' src='" . PICTURES_DIR . "/{$row['picture_filename']}' onclick=\"document.location='project.php?id=$projectId';\">
 				</div>
 				<div class='card-content'>
 					<span class='card-title activator grey-text text-darken-4'  onclick=\"document.location='project.php?id=$projectId';\">{$row['project_name']}
@@ -126,7 +142,7 @@ include('header.inc');
 				</div>
 			</div>
 	      </div>";
-		}
+}
 ?>			
 		</div>
 		<br>
@@ -155,8 +171,7 @@ include('header.inc');
 
 <div class="container">
 	<br>
-	<h5 class="header center brown-text text-lighten-2">Our
-		Impact</h5>
+	<h5 class="header center brown-text text-lighten-2">Our Impact</h5>
 	<div class="section">
 
 		<!--   Icon Section   -->
@@ -202,21 +217,23 @@ include('header.inc');
 				</div>
 			</div>
 		</div>
-		
-			<div>
+
+		<div>
 			<h6 class="light center-align">
 				*based on difference-in-differences analysis using data, from 2014
-				to 2016, on 21 projects and 32 villages (more info <a href='Impacts.php'>here</a>)
+				to 2016, on 21 projects and 32 villages (more info <a
+					href='Impacts.php'>here</a>)
 			</h6>
-			</div>
-			<br>
+		</div>
+		<br>
 	</div>
 
 </div>
 
 <div id="index-banner" class="parallax-container"
 	style="background-color: rgba(0, 0, 0, 0.3); height: 800px">
-	<div class="section no-pad-bot valign-wrapper" style="height: 100%; width:100%;">
+	<div class="section no-pad-bot valign-wrapper"
+		style="height: 100%; width: 100%;">
 		<div class="container">
 			<div class="row center">
 				<h5
@@ -225,30 +242,45 @@ include('header.inc');
 				</h5>
 			</div>
 			<div class="row center">
-				<a href="http://materializecss.com/getting-started.html"
+				<a
 					id="download-button"
-					class="btn-large waves-effect waves-light light blue lighten-1">Give
-					Monthly</a>
-			</div><br>
+					class="btn-large waves-effect waves-light light blue lighten-1 modal-trigger" href="#subscriptionModal" 
+					>Give Monthly</a>
+				<div id="subscriptionModal" class="modal">
+					<div class="modal-content">
+						<h4>Give Monthly</h4>
+						<p>A bunch of text</p>
+					</div>
+					<div class="modal-footer">
+						<form action="/create_subscription.php" method="POST">
+            					<script src="https://checkout.stripe.com/checkout.js"
+            						class="stripe-button" data-key="pk_live_nE8o2Y70d89STcP6UkhSDvxM"
+            						data-image="/images/marketplace.png" data-name="Emma's Farm CSA"
+            						data-description="Subscription for 1 weekly box"
+            						data-amount="2000" data-label="GIVE MONTHLY">
+                             </script>
+            				</form>
+					</div>
+				</div>
+			</div>
+			<br>
 			<div class="row center">
 				<h6
 					class="header col s12 white-text light text-shadow: 4px 4px 7px #FFFFFF;">
-					automatically donate to a project each month,<br> and enjoy impact updates all year long
+					automatically donate to a project each month,<br> and enjoy impact
+					updates all year long
 				</h6>
 			</div>
 			<div class="row center">
 				<h5
 					class="header col s12 white-text light text-shadow: 4px 4px 7px #FFFFFF;">
-					starting at
-				</h5>
+					starting at</h5>
 				<h3
 					class="header col s12 white-text light text-shadow: 4px 4px 7px #FFFFFF;">
-					$5
-				</h3>
+					$5</h3>
 				<h5
 					class="header col s12 white-text light text-shadow: 4px 4px 7px #FFFFFF;">
-					/ month
-				</h5>
+					/ month</h5>
 			</div>
 		</div>
 	</div>
