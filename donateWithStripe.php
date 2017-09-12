bc09e00fdce587a3fa7d4d7154c815d903969f34
Jeff DePree <jdepree@gmail.com>
2017-09-12 17:30:36 -0600

    Some thanks pages updates

--- donateWithStripe.php
+++ donateWithStripe.php
@@ -12,11 +12,12 @@ $donorLastName = param('lastName');
 $amount = param('stripeAmount');
 $projectId = param('projectId');
 $isSubscription = param('isSubscription');
+$token = param('stripeToken');
 
 $result = doQuery("SELECT donor_id FROM donors WHERE donor_email='$donorEmail'");
 if ($row = $result->fetch_assoc()) {
     $donorId = $row['donor_id'];
-    $result = doQuery("SELECT count(donation_id) AS donationCount FROM donations WHERE donation_donor_id=$donorId");
+    $result = doQuery("SELECT count(donation_id) AS donationCount FROM donations WHERE donation_donor_id=$donorId AND donation_stripe_token<>'$token'");
     if ($row = $result->fetch_assoc()) {
         $donationCount = $row['donationCount'] + 1;
     }
@@ -45,19 +46,26 @@ if ($isSubscription) {
              'plan' => $plan
          ));
          $subscriptionId = "'$planName'";
-        sendMail($donorEmail, "Monthly Subscription for Village X", "Thank you for your generous contribution!  Stay tuned for updates to the projects your funds support.<P>The Village X Team</P>");
+        sendMail($donorEmail, "Monthly Subscription for Village X", "Thank you for your generous contribution!  Stay tuned for updates to the projects your funds support.<P>The Village X Team</P>", getAdminEmail());
     } catch (Exception $e) {
         sendMail(getAdminEmail(), "Problem creating subscription", $e->getMessage(), getAdminEmail());
     }
 } else {
-    sendMail($donorEmail, "Donation to Village X", "Thank you for your generous contribution!  Stay tuned for updates to the projects your funds support.<P>The Village X Team</P>");
+    sendMail($donorEmail, "Donation to Village X", "Thank you for your generous contribution!  Stay tuned for updates to the projects your funds support.<P>The Village X Team</P>", getAdminEmail());
 }
 
-doQuery("INSERT INTO donations (donation_donor_id, donation_amount, donation_project_id, donation_subscription_id) VALUES ($donorId, $amount / 100, $projectId, $subscriptionId)");
-if ($projectId) {
-    doQuery("UPDATE projects SET project_funded=project_funded + ($amount / 100) WHERE project_id=$projectId");
+$donationAmountDollars = $amount / 100;
+
+$result = doQuery("SELECT donation_id FROM donations WHERE donation_stripe_token='$token'");
+if ($row = $result->fetch_assoc()) {
+    $donationId = $row['donation_id'];
 } else {
-    include("disburseSubscriptionPayment.php");
+    doQuery("INSERT INTO donations (donation_donor_id, donation_amount, donation_project_id, donation_subscription_id, donation_stripe_token) VALUES ($donorId, $amount / 100, $projectId, $subscriptionId, '$token')");
+    if ($projectId) {
+        doQuery("UPDATE projects SET project_funded=project_funded + ($amount / 100) WHERE project_id=$projectId");
+    } else {
+        include("disburseSubscriptionPayment.php");
+    }
 }
 
 if ($isSubscription) {
