0ecdeb73e137d138ad065997f1b19147f59d5d72
malawi-mike <mike@villagex.org>
2017-09-14 09:31:11 -0400

    added validation to monthly giving form -- need you to figure out why onclick to Stripe is bypassing validation

--- village_fund_payment_view.php
+++ village_fund_payment_view.php
@@ -4,6 +4,25 @@ require_once("utilities.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+<!-- form validation code -->
+<script src="https://jqueryvalidation.org/files/lib/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
+
+<style type="text/css">
+  .input-field div.error{
+    position: relative;
+    top: -1rem;
+    left: 0rem;
+    font-size: 0.8rem;
+    color:#FF4081;
+    -webkit-transform: translateY(0%);
+    -ms-transform: translateY(0%);
+    -o-transform: translateY(0%);
+    transform: translateY(0%);
+  }
+  </style>
+
 <?php include('header.inc');
 
 ?>
@@ -11,7 +30,7 @@ require_once("utilities.php");
            
  <div class="container">
             <br>
-            
+    
             <div id="jquery-validation" class="section" style="display:table; width:100%">
             <div class="col-project valign-wrapper" style="vertical-align: middle;">
             <div class="card" style="border-style:solid; border-width:1px; border-color:blue; border-radius:20px; margin: 0px 0px 0px 0px;">
@@ -19,32 +38,57 @@ require_once("utilities.php");
             <span class="card-title black-text">Give monthly and disrupt extreme poverty all year long</span>
             <div class="row" style="padding:5% 5% 5% 5%;">
             <h6 class="center-align" style="color:blue;">enter an amount and your name</h6>
-            <form class="col s12 formValidate" style="width:100%" id="formValidate" method="get" action="">
+            <form class="col s12 donateForm" style="width:100%" id="donateForm" method="get" action="">
             <div class="row" style="border-style:solid; border-width:2px; border-color:blue; border-radius:20px; padding:3% 3% 3% 3%;">
             <div class="input-field col s12 center-align">
             <i class="material-icons prefix" style="font-size:40px;">attach_money&nbsp;&nbsp;</i>
-            <input placeholder="10" style="font-size:40px; color:blue;" id="donation_amount" type="tel">
+            <input placeholder="10" style="font-size:40px; color:light-blue;" id="donation_amount" type="tel">
             <h5 class="center-align">/ month</h5>
             <div class="input-field col s6">
-            <label for="uname">First Name</label>
-            <input id="donationFirstName" name="uname" type="text" data-error=".errorTxt1">
+            <input id="donationFirstName" name="firstname" placeholder="first name" type="text" required data-error=".errorTxt1">
             <div class="errorTxt1"></div>
             </div>
             <div class="input-field col s6">
-            <label for="last_name">Last Name</label>
-            <input id="donationLastName" name="uname" type="text" data-error=".errorTxt1">
-            <div class="errorTxt1"></div>
+            <input id="donationLastName" name="lastname" placeholder="last name" type="text" required data-error=".errorTxt2">
+            <div class="errorTxt2"></div>
             </div>
             </div>
             </div>
             
             <div class="input-field col s12">
-            <button class="center-align waves-effect waves-light light blue lighten-1 btn-large submit" type="submit" style="width:100%;" onclick="amount = $('#donation_amount').val(); if (!amount) { amount = $('#donation_amount').attr('placeholder'); } donateWithStripe(1, amount * 100, '', 0, $('#donationFirstName').val(), $('#donationLastName').val()); return false;">Donate</button>
-    
+            <button id="donationButton" class="center-align light-blue btn-large submit" type="submit" style="width:100%;"  onclick="amount = $('#donation_amount').val(); if (!amount) { amount = $('#donation_amount').attr('placeholder'); } donateWithStripe(1, amount * 100, '', 0, $('#donationFirstName').val(), $('#donationLastName').val()); return false;"
+            >Donate</button>
             </div>
-           
-            
             </form>
+            
+            <script>
+	$().ready(function() {
+		$("#donateForm").validate({
+			rules: {
+				firstname: "required",
+				lastname: "required",
+			},
+		messages: {
+		      firstname: "this field is required",
+		      lastname: "this field is required",
+		},
+
+        errorElement : 'div',
+        errorPlacement: function(error, element) {
+          var placement = $(element).data('error');
+          if (placement) {
+            $(placement).append(error)
+          } else {
+            error.insertAfter(element);
+          }
+        },
+          submitHandler: function(form) {
+		      form.submit();
+        }	
+		});
+	});
+</script>
+            
             </div>
             </div>
             </div>
