5befa4e3d59a25b071ffd115e72dfd407bf49fd2
Jeff DePree <jdepree@gmail.com>
2017-09-19 23:44:09 -0600

    Cancellation flow

--- cancelStripe.php
+++ cancelStripe.php
@@ -6,9 +6,9 @@ require_once('lib/stripe/init.php');
 // (switch to the live key later)
 \Stripe\Stripe::setApiKey("sk_test_DGoi26vx76FlFn7sen3IDSC8");
 
-$donorEmail = param('email');
+$donorEmail = escStr(param('email'));
 
-$result = doQuery("SELECT donation_subscription_id, donor_first_name, donor_last_name FROM donations JOIN donors ON donation_donor_id=donor_id AND donor_email='$donorEmail'");
+$result = doQuery("SELECT donation_subscription_id, donor_first_name, donor_last_name FROM donations JOIN donors ON donor_email='$donorEmail' AND donation_donor_id=donor_id AND donation_subscription_id IS NOT NULL");
 $count = 0;
 while ($row = $result->fetch_assoc()) {
     $subscription = \Stripe\Subscription::retrieve($row['donation_subscription_id']);
@@ -17,12 +17,12 @@ while ($row = $result->fetch_assoc()) {
 }
 
 if ($count == 0) {
-    print "We were not able to find any subscriptions associated with this email.";
+    print "Sorry, we were not able to find any subscriptions associated with this email ($donorEmail).";
 } else {
-    print "We cancelled $count subscription(s) associated with this email.";
+    print "We cancelled $count subscription(s) associated with this email.  Sorry to lose you!";
     $firstName = $row['donor_first_name'];
     $lastName = $row['donor_last_name'];
-    sendMail($donorEmail, "Subscription Cancelled", "Hi $donorEmail,\n\nWe have cancelled your subscription.\n\nThe Village X Team", getAdminEmail());
+    //sendMail($donorEmail, "Subscription Cancelled", "Hi $donorEmail,\n\nWe have cancelled your subscription.\n\nThe Village X Team", getAdminEmail());
 }
 
 ?>
