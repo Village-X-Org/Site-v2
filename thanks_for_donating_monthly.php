4dfeff35b91aef124874c0512d9c4be179676143
malawi-mike <mike@villagex.org>
2017-09-22 10:35:12 -0400

    rearranged share buttons on thank you for donating pages

--- thanks_for_donating_monthly.php
+++ thanks_for_donating_monthly.php
@@ -29,9 +29,19 @@ include('header.inc'); ?>
 
 <div id="index-banner" class="parallax-container" style="background-color: rgba(0, 0, 0, 0.3); height: 500px;">
 	<div class="section no-pad-bot valign-wrapper" style="height: 100%; width:100%;">
+		<div class="container">
 		<div class="row center">
-			<h2 class="header col s12 white-text text-lighten-2 text-shadow: 2px 2px 7px #111111">Thanks for joining the Village Fund!</h2>	
-
+			<h2 class="header col s12 white-text text-lighten-2 text-shadow: 2px 2px 7px #111111">Thanks for joining the Village Fund!</h2>
+		</div>
+			<br>
+			<div class="row center"><p class="white-text text-lighten-2 text-shadow: 2px 2px 7px #111111"><b>Share your generosity to inspire others</b></p>	
+			</div> 
+			<div class="row center">
+					<?php printShareButtons($projectId, 
+						    "I disrupt extreme poverty every month by funding projects villages choose.", 
+						    "I donate monthly to projects chosen by extreme poverty villages in Africa.", 70); ?>
+			</div>
+	</div>
 			<div class="parallax" style="background-size: cover;">
 				<img src="images/village_family.jpg">
 			</div>
@@ -47,11 +57,6 @@ include('header.inc'); ?>
           <div class="black-text flow-text"><p class="flow-text">
           	<p><?php print $donorFirstName; ?>,</p> 
 			<p>We processed your first monthly donation of $<?php print $donationAmountDollars; ?>! We applied this donation to <?php print $projectName; ?> in <?php print $villageName; ?> Village, <?php print $countryName; ?>. You have disrupted extreme poverty for <?php print $numPeople; ?> people and <?php print $numHouseholds; ?> households.</p>
-			<p class="center-align"><b>Share the good news:</b> 
-						<?php printShareButtons($projectId, 
-						    "I disrupt extreme poverty every month by funding projects villages choose.", 
-						    "I donate monthly to projects chosen by extreme poverty villages in Africa.", 30); ?>
-			</p>
 			<p>We deeply appreciate your commitment to Village X Org. With each montly donation, we'll send you a thank you email identifying the project you've supported.</p> 
 			<p>Please stay tuned for project updates. As soon as they arrive, we'll notify you by email.</p>
         		<p>Sincerely,</p>
