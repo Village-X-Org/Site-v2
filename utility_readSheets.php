1c51dc196b207e0792e0f835f02482517b1aa63c
Jeff Depree <jdepree@gmail.com>
2017-09-24 14:03:03 -0600

    Reading sheets

--- utility_readSheets.php
+++ utility_readSheets.php
@@ -5,10 +5,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 define('APPLICATION_NAME', 'VillageX');
 define('CLIENT_ID', 'fresh-generator-179618');
-define('CREDENTIALS_PATH', '~/.credentials/sheets.googleapis.com-php-quickstart.json');
+define('CREDENTIALS_PATH', __DIR__ . '/.credentials/sheets.googleapis.com-php-quickstart.json');
 define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/sheets.googleapis.com-php-quickstart.json
 define('SCOPES', implode(' ', array(
   Google_Service_Sheets::SPREADSHEETS_READONLY)
 ));
@@ -29,27 +27,21 @@ function getClient() {
   $client->setAuthConfig(CLIENT_SECRET_PATH);
   $client->setAccessType('offline');
 
-  // Load previously authorized credentials from a file.
-  $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
-  if (file_exists($credentialsPath)) {
-    $accessToken = json_decode(file_get_contents($credentialsPath), true);
-  } else {
     // Request authorization from the user.
     $authUrl = $client->createAuthUrl();
     printf("Open the following link in your browser:\n%s\n", $authUrl);
     print 'Enter verification code: ';
     $authCode = trim(fgets(STDIN));
-
+    
     // Exchange authorization code for an access token.
     $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-
+    
     // Store the credentials to disk.
     if(!file_exists(dirname($credentialsPath))) {
       mkdir(dirname($credentialsPath), 0700, true);
     }
     file_put_contents($credentialsPath, json_encode($accessToken));
     printf("Credentials saved to %s\n", $credentialsPath);
-  }
   $client->setAccessToken($accessToken);
 
   // Refresh the token if it's expired.
