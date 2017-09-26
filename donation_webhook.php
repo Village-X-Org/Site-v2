62b01cc226d9aca55034ca7f11266ffb5722c362
Jeff Depree <jdepree@gmail.com>
2017-09-26 11:54:43 -0600

    Actually charge cards with Stripe

--- donation_webhook.php
+++ donation_webhook.php
@@ -7,6 +7,15 @@ $event_json = json_decode($input);
 if ($event_json->type == 'invoice.payment_succeeded') {
     $subscriptionId = $event_json->data->object->subscription;
     $donationAmountDollars = $event_json->data->object->total / 100;
+    
+    \Stripe\Stripe::setApiKey(STRIPE_SECRET_KEY);
+    $charge = \Stripe\Charge::create(array(
+        "amount" => $event_json->data->object->total,
+        "currency" => "usd",
+        "description" => "Subscription Charge",
+        "source" => $token,
+    ));
+    
     $donorId = -1;
     $stmt = prepare("SELECT donation_donor_id FROM donations WHERE donation_subscription_id=? LIMIT 1");
     $stmt->bind_param('s', $subscriptionId);
