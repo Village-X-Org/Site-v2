2a1e210f3418e6b1a8c94d93c57bc1960c37291c
Jeff Depree <jdepree@gmail.com>
2017-10-09 14:59:59 -0400

    Fixing donation webhook - removing charge

--- donation_webhook.php
+++ donation_webhook.php
@@ -8,14 +8,6 @@ if ($event_json->type == 'invoice.payment_succeeded') {
     $subscriptionId = $event_json->data->object->subscription;
     $donationAmountDollars = $event_json->data->object->total / 100;
     
-    \Stripe\Stripe::setApiKey(STRIPE_SECRET_KEY);
-    $charge = \Stripe\Charge::create(array(
-        "amount" => $event_json->data->object->total,
-        "currency" => "usd",
-        "description" => "Subscription Charge",
-        "source" => $token,
-    ));
-    
     $donorId = -1;
     $stmt = prepare("SELECT donation_id, donation_donor_id, donation_code FROM donations WHERE donation_subscription_id=? LIMIT 1");
     $stmt->bind_param('s', $subscriptionId);
