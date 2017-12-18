59c1af53fb503208062947bce695cb743f2687e3
Jeff DePree <jdepree@gmail.com>
2017-12-18 15:24:11 -0700

    Donations since file

--- utility_donationsSince.php
+++ utility_donationsSince.php
@@ -1,6 +1,15 @@
 <?php
 require_once("utilities.php");
 $since = paramInt('since');
+$user = param('u');
+$password = md5(param('p'));
+
+$stmt = prepare("SELECT user_id FROM users WHERE user_username=? AND user_password=?");
+$stmt->bind_param('ss', $user, $password);
+$result = execute($stmt);
+if (!($row = $result->fetch_assoc())) {
+    die(1);
+}
 
 $stmt = prepare("SELECT donation_id, donor_email, donor_first_name, donor_last_name, 
         donation_amount, donation_date, donation_project_id, donation_subscription_id FROM donations 
@@ -9,8 +18,8 @@ $stmt->bind_param('i', $since);
 $result = execute($stmt);
 print "<pre>";
 while ($row = $result->fetch_assoc()) {
-    //print $row['donation_id'].",".$row['donor_email'].",".$row['donor_first_name'].",".$row['donor_last_name'].",".$row['donation_amount']
-    //        .",".$row['donation_date'].",".$row['donation_project_id'].",".$row['donation_subscription_id']."\n";
+    print $row['donation_id'].",".$row['donor_email'].",".$row['donor_first_name'].",".$row['donor_last_name'].",".$row['donation_amount']
+            .",".$row['donation_date'].",".$row['donation_project_id'].",".$row['donation_subscription_id']."\n";
 }
 print "</pre>";
 $stmt->close();
