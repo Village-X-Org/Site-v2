56b0aa750f5b0ea90d51650aadc3fe183cd168bb
Jeff Depree <jdepree@gmail.com>
2018-03-06 14:40:04 -0700

    Ids

--- api/projects.php
+++ api/projects.php
@@ -1,4 +1,5 @@
 <?php
 	require_once("../utilities.php");	
-	print cached('projects.json', doJsonQuery("SELECT project_name, project_lat, project_lng, project_village_id, project_type, project_budget, project_funded FROM projects"));
+	print cached('projects.json', doJsonQuery("SELECT project_id, project_name, project_lat, project_lng, 
+		project_village_id, project_type, project_budget, project_funded, picture_filename FROM projects JOIN pictures ON picture_id=project_profile_image_id"));
 ?>
