432f8bf44a759f9336e0a6d914e9a8ce0beae694
Jeff DePree <jdepree@gmail.com>
2018-05-07 07:25:39 -0600

    Tweaking campaign page styles

--- campaign.php
+++ campaign.php
@@ -13,18 +13,18 @@ require_once("utilities.php");
 	<div class="container no-pad-bot" style="height: 100%; width: 100%; padding: 2% 0% 0% 0%">
 		<div class="row">
 		  <div class="col s6 m6 l2 right-align hide-on-med-and-down" style="padding: 9% 0% 0% 3%;">
-			<div style="opacity:0.7; width:200px; height:200px; border-radius:20%; border-style:solid; background:teal">
-        				<h1 class="header center-align light text-shadow: 2px 2px 7px #111111" style="padding:12% 5% 5% 5%"><b>B</b></h1>
+			<div style="width:200px; height:200px; border-radius:20%; border-style:solid; background:#008080CC;">
+        				<h1 class="header center-align light" style="padding:9% 0 0 0;font-size:96px;"><b>B</b></h1>
 			</div>
 		</div> 
 	
 		<div class="col s6 m6 l6 left-align hide-on-med-and-down" style="padding: 5% 1% 1% 2%;">
 			<div style="padding: 5% 5% 5% 5%">
-				<h3 class="header col s12 white-text text-lighten-2 text-shadow: 2px 2px 7px #111111">Bryan's 35th Birthday</h3>
+				<h3 class="header col s12 white-text text-lighten-2">Bryan's 35th Birthday</h3>
 			</div>
 
 			<div style="padding: 5% 5% 5% 5%;">
-				<h5 class="header light text-shadow: 2px 2px 7px #111111" style="padding:0% 3% 0% 2%">
+				<h5 class="header light" style="padding:0% 3% 0% 2%">
 					Bryan Nelson</h5>
 			</div>
 
@@ -56,7 +56,7 @@ require_once("utilities.php");
 			</div>
 			
 			<div style="padding: 5% 20% 5% 20%;">
-				<h6 class="header light text-shadow: 2px 2px 7px #111111" style="font-weight:thin">fundriasing to <span style="font-size: large; font-weight: bold"><a>Provide Clean Water</a></span></h6>
+				<h6 class="header light text-shadow: 2px 2px 7px #111111" style="font-weight:thin">fundraising to <span style="font-size: large; font-weight: bold">Provide Clean Water</span></h6>
 				<h6>in Saiti Village, Malawi</h6>
 			</div>
 			
@@ -64,29 +64,29 @@ require_once("utilities.php");
 	
 			
 		<div class="col l4 hide-on-med-and-down" style="padding: 1% 3% 1% 1%;">
-      <div class="card teal z-depth-1" style="padding: 1% 1% 1% 1%; opacity:0.6">
+      <div class="card z-depth-1" style="padding: 1% 1% 1% 1%; background:#008080CC;">
         <!-- <div class="card-image">
           <img src="images/woman_with_goat">
           <span class="card-title">Card Title</span>
         </div> -->
         <div class="card-content">
-        <div><h5 class="header white-text text-lighten-2: 2px 2px 7px #111111 center-align" style="opacity:0.7">fundraising to</h5></div>
-          <h3><b>Provide Clean Water</b><span style="font-size: smaller; font-weight: lighter"> in Saiti Village, Malawi</span> <i class="material-icons small">place</i></h3>
-          <p>Helping 1420 people across 178 households. Saiti has contributed $310, materials, and labor.</p>
-        </div>
-        <div class="card-action center-align">
-          <a href="#">meet the village</a>
-        </div>
-      </div>
+	        <h5 class="header white-text text-lighten-2">fundraising to</h5>
+	          <p style='font-size:28px;'><b>Provide Clean Water</b><br/><span style='margin-top:10px;text-size:24px;'> in Saiti Village, Malawi</span></p>
+	          <p style="margin-top:10px;margin-bottom:20px;">Helping 1420 people across 178 households. Saiti has contributed $310, materials, and labor.</p>
+	        <div class="card-action center-align">
+	          <a href="#">meet the village</a>
+	        </div>
+     	</div>
     
-  </div>
-  </div>
+  		</div>
+  		</div>
 
 			<div class="parallax">
 				<img src="images/header1.jpg">
 			</div>
 		</div>
 	</div>
+</div>
 	
 <div class="container">
   	<div class="row" style="width:100%; padding: 2% 0% 2% 0%">
@@ -114,112 +114,33 @@ require_once("utilities.php");
 
 		
 		<div class="center-align" style="padding:8% 2% 2% 2%;">
-		<div style="font-weight:thin; font-size:large">here's a similar project</div>
 		<img src="images/woman_with_goat" class="responsive-img" style='border: black 2px solid; box-shadow: 10px 10px 5px #888888; border-radius:10px;'>	
+		<div style="font-weight:thin; font-size:large">here's a similar project</div>
 		</div>
 		</div>
 		
 		<div class="col s12 m12 l6 left-align" style="vertical-align: middle;padding: 0% 2% 2% 3%">
 						
-      <div class="flow-text more" style="padding: 3% 0% 0% 0%">Many of us have no idea what it's like to be thirsty. We have plenty of water to drink -- even the water in our toilets is clean! But many people around the 
+      <div class="flow-text" style="padding: 3% 0% 0% 0%; font-size:18px;">Many of us have no idea what it's like to be thirsty. We have plenty of water to drink -- even the water in our toilets is clean! But many people around the 
       world don’t have that luxury. Every day, about 1,400 children die from diseases caused by unsafe water and poor sanitation. But it doesn’t have to be that way. There are simple solutions like drilled wells, spring 
       protections and BioSand filters that help provide clean water to communities around the world.</div>
-    
-            <h5 style="padding: 4% 0% 4% 0%">Fundraising Timeline</h5>
-            
-            <div style="overflow: scroll; height:400px">
+            	
+            <h5 style="padding: 2% 0% 0% 0%; font-size:20px;">Fundraising Timeline</h5>
+            <div style="overflow-x:scroll; height:200px;width:100%;">
             <div class="row valign-wrapper">
-            <div style="padding 0 0 0 0%;margin: 0 0% 0 3%; opacity: 0.6; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:100px; width:100px;">
-                 				<a class="tooltip" style='text-decoration:none;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:100px; width:100px; padding: 0 0% 0 0%; font-size: xx-large; font-color: #ffffff; 
+            <div style="padding 0 0 0 0%;margin: 3% 3% 3% 3%; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:80px; width:80px;">
+                 				<a class="tooltip" style='text-decoration:none;color:#EEEEEE;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:80px; width:80px; padding: 0 0% 0 0%; font-size: x-large; font-color: #ffffff; 
                                     text-align: center;display: table-cell;vertical-align:middle;"><b>JD</b></span></a>
                  			
  			</div>
- 			<div style="padding:0 0 0% 5%;vertical-align:middle; display: inline-block;"><span style="font-size: large; font-weight: 300"><b>Tom and Patty Smith</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> donated $50 -- 3 hours ago</span>
+ 			<div style="padding:0 0 0% 5%;vertical-align:middle; display: inline-block;"><span style="font-size: 16px; font-weight: 300"><b>Tom and Patty Smith</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> donated $50</span><br/><span style='text-align:right;'>3 hours ago</span>
  			
- 			<div style="font-size: x-large; font-weight: 200">Happy birthday, Bryan! We love you.</div>
- 			</div>
-					
-		</div>
-		
-		<div class="row valign-wrapper">
-            <div style="padding 0 0 0 0%;margin: 0 0% 0 3%; opacity: 0.6; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:100px; width:100px;">
-                 				<a class="tooltip" style='text-decoration:none;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:100px; width:100px; padding: 0 0% 0 0%; font-size: xx-large; font-color: #ffffff; 
-                                    text-align: center;display: table-cell;vertical-align:middle;"><b>JD</b></span></a>
-                 			
- 			</div>
- 			<div style="padding:0 0 0% 5%;vertical-align:middle; display: inline-block;"><span style="font-size: large; font-weight: 300"><b>Tom and Patty Smith</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> donated $50 -- 3 hours ago</span>
- 			
- 			<div style="font-size: x-large; font-weight: 200">Happy birthday, Bryan! We love you.</div>
- 			</div>
-					
-		</div>
-		
-		<div class="row valign-wrapper">
-            <div style="padding 0 0 0 0%;margin: 0 0% 0 3%; opacity: 0.6; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:100px; width:100px;">
-                 				<a class="tooltip" style='text-decoration:none;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:100px; width:100px; padding: 0 0% 0 0%; font-size: xx-large; font-color: #ffffff; 
-                                    text-align: center;display: table-cell;vertical-align:middle;"><b>JD</b></span></a>
-                 			
- 			</div>
- 			<div style="padding:0 0 0% 5%;vertical-align:middle; display: inline-block;"><span style="font-size: large; font-weight: 300"><b>Tom and Patty Smith</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> donated $50 -- 3 hours ago</span>
- 			
- 			<div style="font-size: x-large; font-weight: 200">Happy birthday, Bryan! We love you.</div>
- 			</div>
-					
-		</div>
-		
-		<div class="row valign-wrapper">
-            <div style="padding 0 0 0 0%;margin: 0 0% 0 3%; opacity: 0.6; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:100px; width:100px;">
-                 				<a class="tooltip" style='text-decoration:none;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:100px; width:100px; padding: 0 0% 0 0%; font-size: xx-large; font-color: #ffffff; 
-                                    text-align: center;display: table-cell;vertical-align:middle;"><b>JD</b></span></a>
-                 			
- 			</div>
- 			<div style="padding:0 0 0% 5%;vertical-align:middle; display: inline-block;"><span style="font-size: large; font-weight: 300"><b>Tom and Patty Smith</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> donated $50 -- 3 hours ago</span>
- 			
- 			<div style="font-size: x-large; font-weight: 200">Happy birthday, Bryan! We love you.</div>
+ 			<div style="font-weight: 200; font-size:16px;">Happy birthday, Bryan! We love you.</div>
  			</div>
 					
 		</div>
 		
 		</div>
-		
-		<script>
-		$(document).ready(function() {
-		    // Configure/customize these variables.
-		    var showChar = 100;  // How many characters are shown by default
-		    var ellipsestext = "...";
-		    var moretext = "Show more";
-		    var lesstext = "Show less";
-		    
-
-		    $('.more').each(function() {
-		        var content = $(this).html();
-		 
-		        if(content.length > showChar) {
-		 
-		            var c = content.substr(0, showChar);
-		            var h = content.substr(showChar, content.length - showChar);
-		 
-		            var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-		 
-		            $(this).html(html);
-		        }
-		 
-		    });
-		 
-		    $(".morelink").click(function(){
-		        if($(this).hasClass("less")) {
-		            $(this).removeClass("less");
-		            $(this).html(moretext);
-		        } else {
-		            $(this).addClass("less");
-		            $(this).html(lesstext);
-		        }
-		        $(this).parent().prev().toggle();
-		        $(this).prev().toggle();
-		        return false;
-		    });
-		});
-		</script>
 		
 	</div>
 </div>
