aa72dbf9fdbc0751ab3c6ecb2091d14b8bc29517
Jeff Depree <jdepree@gmail.com>
2018-05-07 19:02:57 -0600

    Style tweaks

--- user_profile.php
+++ user_profile.php
@@ -33,18 +33,18 @@ require_once("utilities.php");
 	
 			
 		<div class="col l4" style="padding: 5% 3% 1% 1%;">
-      <div class="card z-depth-1" style="padding: 1% 1% 1% 1%; background-color:#008080BB">
+      <div class="card z-depth-1" style="padding: 1% 1% 1% 1%; background-color:#008080BB;">
         <!-- <div class="card-image">
           <img src="images/woman_with_goat">
           <span class="card-title">Card Title</span>
         </div> -->
-        <div class="card-content">
-        <div><h5 class="header white-text center-align" style="padding: 2px 2px 7px;">Bryan's Stats</h5></div>
+        <div class="card-content header white-text light">
+        <div><h5 class="header center-align" style="padding: 2px 2px 7px;">Bryan's Stats</h5></div>
           <div style="padding: 0% 0% 0% 10%">
-          <h5 class="valign-wrapper" style="padding: 3% 3% 1% 0%"><i class="material-icons small">home</i>&nbsp;<span style="font-size: smaller;">Projects: &nbsp;</span><b>2</b></h5>
-          <h5 class="valign-wrapper" style="padding: 1% 3% 1% 0%"><i class="material-icons small">person</i>&nbsp;<span style="font-size: smaller;">People Helped: &nbsp;</span><b>200</b></h5>
-          <h5 class="valign-wrapper" style="padding: 1% 3% 1% 0%"><i class="material-icons small">people</i>&nbsp;<span style="font-size: smaller;">Families Helped: &nbsp;</span><b>35</b></h5>
-          <h5 class="valign-wrapper" style="padding: 1% 3% 1% 0%"><i class="material-icons small">cake</i>&nbsp;<span style="font-size: smaller;">Fundraisers Led: &nbsp;</span><b>0</b></h5>
+          <h5 class="valign-wrapper" style="padding: 3% 3% 1% 0%"><i class="material-icons small">home</i>&nbsp;&nbsp;&nbsp;<span style="font-size: smaller;">Projects: &nbsp;</span><b>2</b></h5>
+          <h5 class="valign-wrapper" style="padding: 1% 3% 1% 0%"><i class="material-icons small">person</i>&nbsp;&nbsp;&nbsp;<span style="font-size: smaller;">People Helped: &nbsp;</span><b>200</b></h5>
+          <h5 class="valign-wrapper" style="padding: 1% 3% 1% 0%"><i class="material-icons small">people</i>&nbsp;&nbsp;&nbsp;<span style="font-size: smaller;">Families Helped: &nbsp;</span><b>35</b></h5>
+          <h5 class="valign-wrapper" style="padding: 1% 3% 1% 0%"><i class="material-icons small">cake</i>&nbsp;&nbsp;&nbsp;<span style="font-size: smaller;">Fundraisers Led: &nbsp;</span><b>0</b></h5>
         	  </div>
         </div>
         
@@ -124,7 +124,7 @@ require_once("utilities.php");
 		</div>		
 		</div>
 
- 		<div class="flow-text more" style="padding: 5% 0% 0% 0%">
+ 		<div class="flow-text" style="padding: 5% 0% 0% 0%">
  		Bryan has supported 2 projects, helping 200 people and 35 households in rural Malawi.  He has donated to water and livestock projects.  Bryan has led 0 fundraisers and is currently not a monthly donor.
           <h5 class="valign-wrapper hide-on-large-only" style="padding: 3% 0% 1% 0%"><i class="material-icons small">home</i>&nbsp;Projects Supported: &nbsp;<b>2</b></h5>
           <h5 class="valign-wrapper hide-on-large-only" style="padding: 1% 0% 1% 0%"><i class="material-icons small">person</i>&nbsp;People Helped: &nbsp;<b>200</b></h5>
@@ -160,193 +160,30 @@ require_once("utilities.php");
 				
 		</div>
 		</div>
-				
-		<!-- <div class="col s12 m12 l6 left-align hide" style="vertical-align: middle;padding: 2% 3% 2% 2%">
-		<div>			
-					<h6>
-							<span style="font-size: xx-large; font-weight: 500">Make your first donation today!</span>
-						</h6>
-						<div class='progress'>
-							<div class='determinate' style='width: 50%'></div>
-						</div>
-						<div class="valign-wrapper"><i class="material-icons small" style="padding:0 2% 0 0%">face</i>join 700 other people who have donated</div>
-<br>
-					
-		<div class="left-align">
-				
-				<a href='project_tiles.php'
-				id="donate-button"
-				class="waves-effect waves-light donor-background lighten-1 btn-large" style="width:100%; border-radius:10px;font-size: large">Donate</a>
-			
-		</div>		
-		</div>
-
-		 <div class="flow-text more" style="padding: 3% 0% 0% 0%">Bryan has supported two projects, helping 200 people and 35 households in rural Malawi.  Bryan has donated to water and livestock projects.  Bryan has led 0 fundraisers.  Bryan is currently not a monthly donor.
-	
-		 </div>
-		 
-		<div style="padding:3% 2% 2% 2%;">
-
-				<h5 class="left-align" style="padding: 4% 0% 4% 0%"><b>Bryan's Impact</b></h5> 
-				<h5 class="center-align"><b><span class="blue-text">Bryan's Villages</span> v. <span style="color:rgba(220,220,220,1)">Control Villages</span></b></h5>
-		
-			<div>
-				<canvas id="chart1" width="350" height="350"></canvas>
-			</div>
-
-			<script>
-				var ctx = document.getElementById("chart1").getContext('2d');
-
-				var chart1 = new Chart(ctx, {
-					type : 'line',
-					data : {
-						labels : [ 2014, 2015, 2016],
-						datasets : [ {
-							label: "Partner Villages Average",
-							fill : false,
-							backgroundColor : "#ffce56",
-							borderColor: "#6495ED",
-                             pointBackgroundColor: "#6495ED",
-                             pointRadius: 5,
-							data : [ 11.92, 12.42, 16.93 ],
-							cubicInterpolationMode: 'monotone',
-						}, 
-
-						{
-							label: "Control Villages Average",
-							fill : false,
-							backgroundColor : "#ffce56",
-							borderColor: "rgba(220,220,220,1)",
-                             pointBackgroundColor: "rgba(220,220,220,1)",
-                             pointRadius: 5,
-							data : [ 13.72, 12.11, 9.5 ],
-							cubicInterpolationMode: 'monotone',
-						}]
-						}, 
-					options : {
-						responsive : true,
-						maintainAspectRatio : false,
-						legend : {
-							display : false,
-						},
-					scales : {
-						yAxes : [ {
-							ticks : {
-								beginAtZero : false,
-							}
-						} ]
-					},
-					}
-
-				});
-			</script>
-	
-	<div class="center-align" style="padding: 2% 0 0 0; font-weight:thin; font-size:medium">dev scores based on 17 indicators collected from each village for health, education, business, lifestyle, agriculture, and livestock</div>
-	
-		</div>
-		</div>   -->
 		
 		<div class="col s12 m12 l6 left-align" style="vertical-align: middle;padding: 0% 2% 2% 3%">
 	
-            <h5 class="valign-wrapper" style="padding: 4% 0% 4% 0%"><b>Bryan's Donations</b>&nbsp;<span style="font-size: smaller; font-weight: lighter">(Total:&nbsp;$100)</span></h5>
+            <h5 class="valign-wrapper" style="padding: 4% 0% 4% 0%"><b>Bryan's Donations</b>&nbsp;<span style="font-size: smaller; font-weight: lighter;">(Total:&nbsp;$100)</span></h5>
         
-            <div style="overflow: scroll; height:250px">
-            <div class="row valign-wrapper">
-            <div class="col s4 center-align">
-            <div style="padding 0 0 0 0%;margin: 0 0% 0 3%; opacity: 0.6; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:100px; width:100px;">
-                 				<a class="tooltip" style='text-decoration:none;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:100px; width:100px; padding: 0 0% 0 0%; font-size: xx-large; font-color: #ffffff; 
-                                    text-align: center;display: table-cell;vertical-align:middle;"><b>BN</b></span></a>
-                 			
- 			</div>
- 			</div>
- 			
- 			<div class="col s8" >
- 			<div style="padding:0 0 0% 0%;vertical-align:middle; display: inline-block;"><span style="font-size: large; font-weight: 300"><b>Bryan donated $15</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> on Dec 15, 2017 to</span>
- 			
- 			<span style="font-size: x-large; font-weight: 200"><a href="">Provide Clean Water</a> in Saiti Village</span>
- 			</div>
- 			</div>
+            <div style="overflow: scroll; height:250px;">
+            	<div class="row valign-wrapper">
+		            <div style="padding 0 0 0 0%;margin: 3% 0% 3% 3%; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:80px; width:80px;">
+		                 				<a class="tooltip" style='text-decoration:none;color:#EEEEEE;'><span style="height:80px; width:80px; padding: 0 0% 0 0%; font-size: x-large; font-color: #ffffff; 
+		                                    text-align: center;display: table-cell;vertical-align:middle;"><b>BN</b></span></a>
+		                 			
+		 			</div>
+		 			<div style="padding:0 0 0% 5%;vertical-align:middle; display: inline-block;"><span style="font-size: 16px; font-weight: 300"><b>Bryan donated $15</b><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> on Dec 15, 2017 to</span>
+		 				<div style="font-weight: 200; font-size:16px;"><a href="">Provide Clean Water</a> in Saiti Village</div>
+		 			</div>
+				</div>
+			</div>
+            
+				
 					
-		</div>
-		
-		<div class="row valign-wrapper">
-            <div class="col s4 center-align">
-            <div style="padding 0 0 0 0%;margin: 0 0% 0 3%; opacity: 0.6; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:100px; width:100px;">
-                 				<a class="tooltip" style='text-decoration:none;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:100px; width:100px; padding: 0 0% 0 0%; font-size: xx-large; font-color: #ffffff; 
-                                    text-align: center;display: table-cell;vertical-align:middle;"><b>BN</b></span></a>
-                 			
- 			</div>
- 			</div>
- 			
- 			<div class="col s8">
- 			<div style="padding:0 0 0% 0%;vertical-align:middle; display: inline-block;"><span style="font-size: large; font-weight: 300"><b>Bryan donated $15</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> on Dec 15, 2017 to</span>
- 			
- 			<span style="font-size: x-large; font-weight: 200"><a href="">Provide Clean Water</a> in Saiti Village</span>
- 			</div>
- 			</div>
-					
-		</div>
-		
-		<div class="row valign-wrapper">
-            <div class="col s4 center-align">
-            <div style="padding 0 0 0 0%;margin: 0 0% 0 3%; opacity: 0.6; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:100px; width:100px;">
-                 				<a class="tooltip" style='text-decoration:none;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:100px; width:100px; padding: 0 0% 0 0%; font-size: xx-large; font-color: #ffffff; 
-                                    text-align: center;display: table-cell;vertical-align:middle;"><b>BN</b></span></a>
-                 			
- 			</div>
- 			</div>
- 			
- 			<div class="col s8">
- 			<div style="padding:0 0 0% 0%;vertical-align:middle; display: inline-block;"><span style="font-size: large; font-weight: 300"><b>Bryan donated $15</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> on Dec 15, 2017 to</span>
- 			
- 			<span style="font-size: x-large; font-weight: 200"><a href="">Provide Clean Water</a> in Saiti Village</span>
- 			</div>
- 			</div>
-					
-		</div>
-		
-		<div class="row valign-wrapper">
-            <div class="col s4 center-align">
-            <div style="padding 0 0 0 0%;margin: 0 0% 0 3%; opacity: 0.6; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:100px; width:100px;">
-                 				<a class="tooltip" style='text-decoration:none;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:100px; width:100px; padding: 0 0% 0 0%; font-size: xx-large; font-color: #ffffff; 
-                                    text-align: center;display: table-cell;vertical-align:middle;"><b>BN</b></span></a>
-                 			
- 			</div>
- 			</div>
- 			
- 			<div class="col s8">
- 			<div style="padding:0 0 0% 0%;vertical-align:middle; display: inline-block;"><span style="font-size: large; font-weight: 300"><b>Bryan donated $15</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> on Dec 15, 2017 to</span>
- 			
- 			<span style="font-size: x-large; font-weight: 200"><a href="">Provide Clean Water</a> in Saiti Village</span>
- 			</div>
- 			</div>
-					
-		</div>
-		
-		<div class="row valign-wrapper">
-            <div class="col s4 center-align">
-            <div style="padding 0 0 0 0%;margin: 0 0% 0 3%; opacity: 0.6; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:100px; width:100px;">
-                 				<a class="tooltip" style='text-decoration:none;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:100px; width:100px; padding: 0 0% 0 0%; font-size: xx-large; font-color: #ffffff; 
-                                    text-align: center;display: table-cell;vertical-align:middle;"><b>BN</b></span></a>
-                 			
- 			</div>
- 			</div>
- 			
- 			<div class="col s8">
- 			<div style="padding:0 0 0% 0%;vertical-align:middle; display: inline-block;"><span style="font-size: large; font-weight: 300"><b>Bryan donated $15</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> on Dec 15, 2017 to</span>
- 			
- 			<span style="font-size: x-large; font-weight: 200"><a href="">Provide Clean Water</a> in Saiti Village</span>
- 			</div>
- 			</div>
-					
-		</div>
-					
-		</div>
-		
 		<div style="padding:3% 2% 2% 2%;">
 
 				<h5 class="left-align" style="padding: 4% 0% 4% 0%"><b>Bryan's Impact</b></h5> 
-				<h5 class="center-align"><b><span class="blue-text">Bryan's Villages</span> v. <span style="color:rgba(220,220,220,1)">Control Villages</span></b></h5>
+				<h5 class="center-align"><b><span class="blue-text">Bryan's Villages</span> v. <span style="color:rgba(150,75,75,.7);">Control Villages</span></b></h5>
 		<div>
 				<canvas id="chart3" width="250" height="250"></canvas>
 			</div>
@@ -373,8 +210,8 @@ require_once("utilities.php");
 							label: "Control Villages Average",
 							fill : false,
 							backgroundColor : "#ffce56",
-							borderColor: "rgba(220,220,220,1)",
-                             pointBackgroundColor: "rgba(220,220,220,1)",
+							borderColor: "rgba(150,75,75,.7)",
+                             pointBackgroundColor: "rgba(150,75,75,.7)",
                              pointRadius: 5,
 							data : [ 13.72, 12.11, 9.5 ],
 							cubicInterpolationMode: 'monotone',
@@ -401,52 +238,38 @@ require_once("utilities.php");
 			
 		</div>
 		</div>
-		
-		<script>
-		$(document).ready(function() {
-		    // Configure/customize these variables.
-		    var showChar = 100;  // How many characters are shown by default
-		    var ellipsestext = "...";
-		    var moretext = "Show more";
-		    var lesstext = "Show less";
-		    
-
-		    $('.more').each(function() {
-		        var content = $(this).html();
-		 
-		        if(content.length > showChar) {
-		 
-		            var c = content.substr(0, showChar);
-		            var h = content.substr(showChar, content.length - showChar);
-		 
-		            var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-		 
-		            $(this).html(html);
-		        }
-		 
-		    });
-		 
-		    $(".morelink").click(function(){
-		        if($(this).hasClass("less")) {
-		            $(this).removeClass("less");
-		            $(this).html(moretext);
-		        } else {
-		            $(this).addClass("less");
-		            $(this).html(lesstext);
-		        }
-		        $(this).parent().prev().toggle();
-		        $(this).prev().toggle();
-		        return false;
-		    });
-		});
-		</script>
-		
 	</div>
 </div>
 
-<div class="center-align flow-text">see code below for sample of carousel of pics from user's projects</div>
-
-<!-- add carousel of select pics from user's projects-->
+    <?php
+        $stmt = prepare("SELECT picture_filename, pu_description FROM project_updates JOIN pictures ON pu_project_id=? AND pu_image_id=picture_id ORDER BY pu_timestamp ASC");
+        $stmt->bind_param('i', $projectId);
+        $result = execute($stmt);
+        $count = 0;
+        while ($row = $result->fetch_assoc()) {
+            if ($count == 0) {
+                print "<hr width='85%'><div id='pics' class='section scrollspy'>
+				           <h5 style='text-align: center; color:#4FC3F7; font-weight:300;'>Field Updates</h5>
+                            ".($completion ? "<span class='flow-text align-center' style='font-size:16px;'>".strip_tags($completion)."</span>" : "")."
+                                <div class='carousel'>";
+            }
+            print "<a class='carousel-item' href='' onclick=\"$('#pictureCaption').text('".addslashes($row['pu_description'])."'); return false;\"><img src='".PICTURES_DIR."{$row['picture_filename']}' /></a>";
+            $count++;
+        }
+        $stmt->close();
+        if ($count > 0) {
+            ?>
+                  <script>
+                  $(document).ready(function(){
+                      $('.carousel').carousel();
+                    });
+                  </script>
+                  </div>
+                  
+                <h6 style="text-align: center" id='pictureCaption'>(swipe to view on mobile)</h6>
+            <?php 
+        }
+    ?>
         
 </div>
 </div>
