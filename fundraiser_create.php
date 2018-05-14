b89f07e9f8f6e3c54cc1861ae277317497636751
Jeff Depree <jdepree@gmail.com>
2018-05-14 15:43:25 -0600

    Fundraiser definition and view

---
+++ fundraiser_create.php
@@ -0,0 +1,221 @@
+<?php
+require_once("utilities.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Village X Org | Fund Projects That Villages Choose</title>
+<meta name="description" content="Disrupting extreme poverty in rural Africa with democracy, direct giving, and data."/>
+<style>
+body, html {
+    height: 100%;
+    margin: 0;
+}
+
+.bg {
+    /* The image used */
+    background-image: url("images/newsletter_banner_2.jpg");
+
+    /* Full height */
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+}
+
+
+</style>
+<?php include('header.inc'); ?>
+
+<div class="bg valign-wrapper">
+
+<div class="container" style="padding:0 15% 0 15%">
+<div class="white-text container flow-text center-align"><b>READY.  SET.  FUNDRAISE.</b></div>
+	<div class="section" id="jqueryvalidation" style="width:100%">
+		
+			<div class="card donor-border" style="border-style:solid; border-width:3px; border-radius:20px; border-color: black; margin: 0px 0px 0px 0px;">
+            		<div class="card-content donor-text" style="height:100%;">
+            		
+         				<div class="row donor-text" style="padding:5% 0% 0% 0%;">
+          				
+         				<form class="col s12" style="width:100%" id="fundraiser_form" method='post' action="">
+                         
+         						<div class="row" style="padding:0% 3% 0 3%;margin:0;">
+         						<div class="black-text" style="font-size:large; padding:0 0 0 3%"><b>1. FUNDRAISER NAME</b></div>
+         						<div class="input-field col s12 donor-text">
+          							<input placeholder="e.g., Sally's 25th Birthday" class='text' type="text" style="padding:0% 1% 1% 1%; font-size:20px; border-style:solid; border-width:thin;border-radius:5px" id="campaign_name" required data-error=".errorTxt1"/>
+          							<div class="errorTxt1 center-align" style="padding:0 0 3% 0; font-size:10px; color:red;"></div>
+          						</div>
+          						
+          						</div>
+          						
+          						
+                             
+                             <div class="row" style="padding:2% 3% 0 3%;margin:0;">
+                             <div class="black-text" style="font-size:large; padding:0 0 0 3%"><b>2. CHOOSE A PROJECT</b></div>
+                                <div class="input-field col s12 donor-text" style="padding:0% 8% 1% 8%; font-size:20px;">
+            	                        <select name='projectSelect' id="project_choice" required data-error=".errorTxt1">
+            	                        	  
+            	                          <option data-icon="images/borehole_donors.jpg" class="left circle truncate">Start a Goat Herd in Saiti (20% funded)</option>
+            	                          <option data-icon="images/building_school.jpg" class="left circle">Provide Clean Water in Mlenga (30% funded)</option>
+            	                          <option data-icon="images/building_school.jpg" class="left circle">Build a Nursery School in Likoswe (10% funded)</option>
+            	                          <option data-icon="images/building_school.jpg" class="left circle">Grow More Food in Kazembe (60% funded)</option>
+            	                        </select>
+            	                     
+	                 			</div>
+	                 			
+	                 			<div class="center-align black-text" style="font-size: small;">This project needs $____.</div>
+	                 			
+	                 			</div>
+	                 			
+	                 			  
+	                 			
+	                 			<script>
+	                 $(document).ready(function() {
+	          		    $('select').material_select();
+	          			});
+	            	    </script>
+	                 			
+	                 		
+              
+                            <div class="row valign-wrapper hide-on-med-and-down" style="padding:7% 3% 0 3%;">
+                            <div class="col s6">
+                            <div class="black-text center-align" style="font-size:large; padding:0 0 0 3%"><b>3. FUNDING GOAL</b></div>
+         						<div class="input-field col s12 donor-text">	
+         							<i class="material-icons prefix left-align" style="font-size:30px">attach_money</i>
+          							<input placeholder="350" class='donor-text' style="font-size:35px;" id="campaign_goal"/>
+          						</div>
+          					
+          					</div>
+          					
+          					<div class="col s6">
+          					<div class="black-text center-align" style="font-size:large; padding:0 0 0 3%"><b>4. ENDING WHEN?</b></div>
+          						<div class="input-field col s12">	
+          							<i class="material-icons prefix left-align" style="font-size:30px">date_range</i>
+	          						<input type="text" style="font-size:20px;" class="datepicker" placeholder="e.g., March 20" name="fundraiserEndDate" id="end_date" required data-error=".errorTxt3">
+          						</div>
+          						<div class="errorTxt3 center-align" style="font-size:10px; color:red;"></div>
+          					</div>
+          					</div>
+          					
+          					
+          					<div class="row valign-wrapper hide-on-large-only" style="padding:7% 3% 0 3%;">
+                            <div class="col s12">
+                            <div class="black-text left-align" style="font-size:large; padding:0 0 0 3%"><b>3. FUNDING GOAL</b></div>
+         						<div class="input-field col s12 donor-text">	
+         							<i class="material-icons prefix left-align" style="font-size:30px">attach_money</i>
+          							<input placeholder="350" class='donor-text' style="font-size:35px;" id="campaign_goal"/>
+          						</div>
+          					
+          					</div>
+          					</div>
+          					
+          					<div class="row valign-wrapper hide-on-large-only" style="padding:7% 3% 0 3%;">
+          					<div class="col s12">
+          					<div class="black-text left-align" style="font-size:large; padding:0 0 0 3%"><b>4. ENDING WHEN?</b></div>
+          						<div class="input-field col s12">	
+          							<i class="material-icons prefix left-align" style="font-size:30px">date_range</i>
+	          						<input type="text" style="font-size:20px;" class="datepicker" placeholder="e.g., March 20" name="fundraiserEndDate" id="end_date" required data-error=".errorTxt3">
+          						</div>
+          						<div class="errorTxt3 center-align" style="font-size:10px; color:red;"></div>
+          					</div>
+          					</div>
+          		
+          		
+	          		<script>$('.datepicker').pickadate({
+	          		    selectMonths: true,
+	          		    selectYears: 2,
+	          		    clear: 'Clear',
+	          		    close: 'Select',
+	          		    today: '',
+                        min: new Date(),
+	          		    closeOnSelect: true
+	          		  });
+	          		</script>
+	          		
+	          		<div class="row" style="padding:0 5% 0 5%;margin:0;">
+	          		<div class="black-text center-align hide-on-med-and-down" style="font-size:large; padding:0 0 0% 3%"><b>5. TELL YOUR STORY</b></div>
+	          		<div class="black-text left-align hide-on-large-only" style="font-size:large; padding:0 0 0% 3%"><b>5. TELL YOUR STORY</b></div>
+        							<div class="input-field col s12 donor-text hide-on-med-and-down" style="padding:0% 1% 0% 1%;">
+         						 	<textarea id="describe_fundraiser" class="materialize-textarea" data-length="300" style="font-size:20px;" placeholder="OPTIONAL: Add a few sentences about what inspired you and why people should give."></textarea>
+        							</div>
+        							<div class="input-field col s12 donor-text hide-on-large-only" style="padding:0% 1% 0% 1%;">
+         						 	<textarea id="describe_fundraiser" class="materialize-textarea" data-length="300" style="font-size:16px;" placeholder="OPTIONAL: Share what inspired you to fundraise."></textarea>
+        							</div>
+        							
+        							<script>$(document).ready(function(){
+
+        								  //This does set the label to the active position.
+        								  setTimeout(function(){ $('.input-field label').addClass('active'); }, 1);
+        								  
+        								});</script>
+        							
+      				</div>   
+	          		
+          			
+                              </div>
+                        
+                           <div class="center-align valign-wrapper hide-on-med-and-down" style="width:100%; padding:0 3% 0% 3%;">
+                    		   <div class="input-field center-align" style="width:100%;">
+                    		   		
+                    				<button id="donationButton" class="btn-large donor-background center-align submit" type="submit" style="width:100%;height:70px;font-size:25px"> 
+                    					Create Fundraiser 
+                    				</button>
+                    			</div>
+                    			</div>
+                    			
+                    		<div class="center-align valign-wrapper hide-on-large-only" style="width:100%; padding:0 3% 0% 3%;">		
+                    		   <div class="input-field center-align" style="width:100%;">	
+                    				<button id="donationButton" class="btn-large donor-background center-align submit" type="submit" style="width:100%;height:70px;font-size:25px"> 
+                    					Create 
+                    				</button>
+            				   </div>
+            				   </div>
+            				   </div>
+            				  
+              			</form>
+<script>
+	$().ready(function() {
+		$("#fundraiser_form").validate({
+			rules: {
+				firstname: "required",
+				lastname: "required",
+				end_date: "required",
+			},
+		messages: {
+		      firstname: "this field is required",
+		      lastname: "this field is required",
+		      end_date: "this field is required",
+		},
+
+		errorElement : 'div',
+     	errorPlacement: function(error, element) {
+       		var placement = $(element).data('error');
+       		if (placement) {
+         		$(placement).append(error)
+       		} else {
+         		error.insertAfter(element);
+       		}
+     	},
+        submitHandler: function(form) {
+        	$.post( "travel_request_info.php", $( "#fundraiser_form" ).serialize())
+        			.done(function( data ) {
+					$( "#travelInfoRequestDiv" ).html( data );
+					document.getElementById("travelInfoRequestDiv").scrollIntoView();
+			});
+        }	
+		});
+	});
+</script>
+
+        			</div>
+			</div>
+		</div>
+	
+	</div>
+</div>
+
+
+</div>
---
+++ fundraiser_view.php
@@ -0,0 +1,151 @@
+<?php
+require_once("utilities.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Village X Org | Fund Projects That Villages Choose</title>
+<meta name="description" content="Disrupting extreme poverty in rural Africa with democracy, direct giving, and data."/>
+<?php include('header.inc'); ?>
+
+<div id="index-banner" class="parallax-container" style="background-color: rgba(0, 0, 0, 0.3); height: 500px">
+    
+	<div class="container no-pad-bot" style="height: 100%; width: 100%; padding: 2% 0% 0% 0%">
+		<div class="row">
+		  <div class="col s6 m6 l2 right-align hide-on-med-and-down" style="padding: 9% 0% 0% 3%;">
+			<div style="width:200px; height:200px; border-radius:20%; border-style:solid; background:#008080CC;">
+        				<h1 class="header center-align light" style="padding:9% 0 0 0;font-size:96px;"><b>B</b></h1>
+			</div>
+		</div> 
+	
+		<div class="col s6 m6 l6 left-align hide-on-med-and-down" style="padding: 5% 1% 1% 2%;">
+			<div style="padding: 5% 5% 5% 5%">
+				<h3 class="header col s12 white-text text-lighten-2">Bryan's 35th Birthday</h3>
+			</div>
+
+			<div style="padding: 5% 5% 5% 5%;">
+				<h5 class="header light" style="padding:0% 3% 0% 2%">
+					Bryan Nelson</h5>
+			</div>
+
+			<div style="padding: 0% 5% 5% 7%;">
+				<br>
+				<br>
+				<a href="project_tiles.php" id="download-button"
+					class="btn-large waves-effect waves-light light blue lighten-1" style="border-radius:10px;">donate</a>
+			</div>
+			
+			</div>
+		
+		
+			<div class="col s12 m12 l6 center-align hide-on-large-only" style="padding: 12% 1% 1% 1%;">
+			<div style="padding: 5% 5% 5% 5%">
+				<h3 class="header col s12 white-text text-lighten-2 text-shadow: 2px 2px 7px #111111">Bryan's 35th Birthday</h3>
+			</div>
+
+			<div style="padding: 5% 5% 5% 5%;">
+				<h5 class="header light text-shadow: 2px 2px 7px #111111" style="padding:0% 3% 0% 3%">
+					Bryan Nelson</h5>
+			</div>
+
+			<div style="padding: 0% 5% 5% 5%;">
+				<br>
+				<br>
+				<a href="project_tiles.php" id="download-button"
+					class="btn-large waves-effect waves-light light blue lighten-1" style="border-radius:10px;">donate</a>
+			</div>
+			
+			<div style="padding: 5% 20% 5% 20%;">
+				<h6 class="header light text-shadow: 2px 2px 7px #111111" style="font-weight:thin">fundraising to <span style="font-size: large; font-weight: bold">Provide Clean Water</span></h6>
+				<h6>in Saiti Village, Malawi</h6>
+			</div>
+			
+			</div>
+	
+			
+		<div class="col l4 hide-on-med-and-down" style="padding: 1% 3% 1% 1%;">
+      <div class="card z-depth-1" style="padding: 1% 1% 1% 1%; background:#008080CC;">
+        <!-- <div class="card-image">
+          <img src="images/woman_with_goat">
+          <span class="card-title">Card Title</span>
+        </div> -->
+        <div class="card-content">
+	        <h5 class="header white-text text-lighten-2">fundraising to</h5>
+	          <p style='font-size:28px;'><b>Provide Clean Water</b><br/><span style='margin-top:10px;text-size:24px;'> in Saiti Village, Malawi</span></p>
+	          <p style="margin-top:10px;margin-bottom:20px;">Helping 1420 people across 178 households. Saiti has contributed $310, materials, and labor.</p>
+	        <div class="card-action center-align">
+	          <a href="#">meet the village</a>
+	        </div>
+     	</div>
+    
+  		</div>
+  		</div>
+
+			<div class="parallax">
+				<img src="images/header1.jpg">
+			</div>
+		</div>
+	</div>
+</div>
+	
+<div class="container">
+  	<div class="row" style="width:100%; padding: 2% 0% 2% 0%">
+  	     
+				
+		<div class="col s12 m12 l6 left-align" style="vertical-align: middle;padding: 2% 3% 2% 2%">
+							
+					<h6>
+							<b><span style="font-size: xx-large; font-weight: bold">$200</span> <span style="font-size: x-large; font-weight: 300">funded out of $400</span></b>
+						</h6>
+						<div class='progress'>
+							<div class='determinate' style='width: 50%'></div>
+						</div>
+						<div class="valign-wrapper"><i class="material-icons small" style="padding:0 2% 0 0%">access_time</i>10 days left to contribute</div>
+<br>
+					
+		<div class="left-align">
+				
+				<a href=''
+				id="donate-button"
+				class="waves-effect waves-light donor-background lighten-1 btn-large" style="width:100%; border-radius:10px;font-size: large">Donate</a>
+			
+				
+		</div>
+
+		
+		<div class="center-align" style="padding:8% 2% 2% 2%;">
+		<img src="images/woman_with_goat" class="responsive-img" style='border: black 2px solid; box-shadow: 10px 10px 5px #888888; border-radius:10px;'>	
+		<div style="font-weight:thin; font-size:large">here's a similar project</div>
+		</div>
+		</div>
+		
+		<div class="col s12 m12 l6 left-align" style="vertical-align: middle;padding: 0% 2% 2% 3%">
+						
+      <div class="flow-text" style="padding: 3% 0% 0% 0%; font-size:18px;">Many of us have no idea what it's like to be thirsty. We have plenty of water to drink -- even the water in our toilets is clean! But many people around the 
+      world don’t have that luxury. Every day, about 1,400 children die from diseases caused by unsafe water and poor sanitation. But it doesn’t have to be that way. There are simple solutions like drilled wells, spring 
+      protections and BioSand filters that help provide clean water to communities around the world.</div>
+            	
+            <h5 style="padding: 2% 0% 0% 0%; font-size:20px;">Fundraising Timeline</h5>
+            <div style="overflow-x:scroll; height:200px;width:100%;">
+            <div class="row valign-wrapper">
+            <div style="padding 0 0 0 0%;margin: 3% 3% 3% 3%; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:80px; width:80px;">
+                 				<a class="tooltip" style='text-decoration:none;color:#EEEEEE;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:80px; width:80px; padding: 0 0% 0 0%; font-size: x-large; font-color: #ffffff; 
+                                    text-align: center;display: table-cell;vertical-align:middle;"><b>JD</b></span></a>
+                 			
+ 			</div>
+ 			<div style="padding:0 0 0% 5%;vertical-align:middle; display: inline-block;"><span style="font-size: 16px; font-weight: 300"><b>Tom and Patty Smith</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> donated $50</span><br/><span style='text-align:right;'>3 hours ago</span>
+ 			
+ 			<div style="font-weight: 200; font-size:16px;">Happy birthday, Bryan! We love you.</div>
+ 			</div>
+					
+		</div>
+		
+		</div>
+		
+	</div>
+</div>
+	
+</div>
+</div>
+
+<?php include('footer.inc'); ?>
