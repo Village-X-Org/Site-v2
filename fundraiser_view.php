9d4510d66620aeb51d0dd1c58f2422c8c2c40e98
Jeff Depree <jdepree@gmail.com>
2018-05-15 08:30:53 -0600

    Fundraiser data updates

--- fundraiser_view.php
+++ fundraiser_view.php
@@ -7,22 +7,25 @@ if (!hasParam('id')) {
 }
 $id = param('id');
 
-$stmt = prepare("SELECT fundraiser_title, fundraiser_subject, fundraiser_amount, fundraiser_description, fundraiser_deadline, project_name, village_name, country_label,
-		project_budget, vs1.stat_value AS peopleCount, vs2.stat_value AS houseCount, picture_filename
+$stmt = prepare("SELECT fundraiser_title, donor_first_name, donor_last_name, fundraiser_amount, fundraiser_description, 
+		UNIX_TIMESTAMP(fundraiser_deadline) AS fundraiser_deadline, project_name, village_name, country_label,
+		project_id, project_budget, vs1.stat_value AS peopleCount, vs2.stat_value AS houseCount, picture_filename
 		FROM fundraisers JOIN projects ON fundraiser_project_id=project_id 
 		JOIN villages ON project_village_id=village_id 
 		JOIN countries ON village_country=country_id
 		JOIN pictures ON project_similar_image_id=picture_id
+		JOIN donors ON fundraiser_subject=donor_id
 		LEFT JOIN village_stats AS vs1 ON vs1.stat_village_id=village_id AND vs1.stat_type_id=18 AND YEAR(project_date_posted)=vs1.stat_year
         LEFT JOIN village_stats AS vs2 ON vs2.stat_village_id=village_id AND vs2.stat_type_id=19 AND YEAR(project_date_posted)=vs2.stat_year
 		WHERE fundraiser_id=?");
 $stmt->bind_param('i', $id);
 $result = execute($stmt);
 if ($row = $result->fetch_assoc()) {
+	$projectId = $row['project_id'];
 	$title = $row['fundraiser_title'];
 	$amount = $row['fundraiser_amount'];
 	$description = $row['fundraiser_description'];
-	$subject = $row['fundraiser_subject'];
+	$subject = $row['donor_first_name'].' '.$row['donor_last_name'];
 	$deadline = $row['fundraiser_deadline'];
 	$projectName = $row['project_name'];
 	$villageName = $row['village_name'];
@@ -31,6 +34,31 @@ if ($row = $result->fetch_assoc()) {
 	$houseCount = $row['houseCount'];
 	$similarPicture = $row['picture_filename'];
 	$villageContribution = round($row['project_budget'] * .05);
+
+	$stmt->close();
+
+	$totalDonationAmount = 0;
+	$donationNames = array();
+	$donationInitials = array();
+	$donationAmounts = array();
+	$donationDates = array();
+	$donationMessages = array();
+	$stmt = prepare("SELECT donor_first_name, donor_last_name, donation_amount, UNIX_TIMESTAMP(donation_date) AS donation_date, donation_message 
+		FROM donations JOIN donors ON donation_donor_id=donor_id WHERE donation_fundraiser_id=?");
+	$stmt->bind_param('i', $id);
+	$result = execute($stmt);
+	while ($row = $result->fetch_assoc()) {
+		$nextAmount = $row['donation_amount'];
+		$firstName = $row['donor_first_name'];
+		$lastName = $row['donor_last_name'];
+    	$donationInitials[] = $firstName[0].(strlen($lastName) > 0 ? $lastName[0] : "");
+		$donationNames[] = $firstName.(strlen($lastName) > 0 ? ' '.$lastName : "");
+		$donationAmounts[] = $nextAmount;
+		$totalDonationAmount += $nextAmount;
+		$donationDates[] = $row['donation_date'];
+		$donationMessages[] = $row['donation_message'];
+	}
+	$stmt->close();
 } else {
 	print "Fundraiser not found";
 	return;
@@ -66,7 +94,7 @@ if ($row = $result->fetch_assoc()) {
 			<div style="padding: 0% 5% 5% 7%;">
 				<br>
 				<br>
-				<a href="project_tiles.php" id="download-button"
+				<a href="one_time_payment_view.php?fundraiserId=<?php print $id; ?>" id="download-button"
 					class="btn-large waves-effect waves-light light blue lighten-1" style="border-radius:10px;">donate</a>
 			</div>
 			
@@ -100,16 +128,12 @@ if ($row = $result->fetch_assoc()) {
 			
 		<div class="col l4 hide-on-med-and-down" style="padding: 1% 3% 1% 1%;">
       <div class="card z-depth-1" style="padding: 1% 1% 1% 1%; background:#008080CC;">
-        <!-- <div class="card-image">
-          <img src="images/woman_with_goat">
-          <span class="card-title">Card Title</span>
-        </div> -->
         <div class="card-content">
 	        <h5 class="header white-text text-lighten-2">fundraising to</h5>
 	          <p style='font-size:28px;'><b><?php print $projectName; ?></b><br/><span style='margin-top:10px;text-size:24px;'> in <?php print $villageName; ?> Village, <?php print $countryName; ?></span></p>
 	          <p style="margin-top:10px;margin-bottom:20px;">Helping <?php print $peopleCount; ?> people across <?php print $houseCount; ?> households. <?php print $villageName; ?> has contributed $<?php print $villageContribution; ?>, materials, and labor.</p>
 	        <div class="card-action center-align">
-	          <a href="#">meet the village</a>
+	          <a href="project.php?id=<?php print $projectId; ?>">meet the village</a>
 	        </div>
      	</div>
     
@@ -130,17 +154,29 @@ if ($row = $result->fetch_assoc()) {
 		<div class="col s12 m12 l6 left-align" style="vertical-align: middle;padding: 2% 3% 2% 2%">
 							
 					<h6>
-							<b><span style="font-size: xx-large; font-weight: bold">$200</span> <span style="font-size: x-large; font-weight: 300">funded out of $400</span></b>
+							<b><span style="font-size: xx-large; font-weight: bold">$<?php print $totalDonationAmount; ?></span> &nbsp;<span style="font-size: x-large; font-weight: 300">funded out of $<?php print $amount; ?></span></b>
 						</h6>
 						<div class='progress'>
-							<div class='determinate' style='width: 50%'></div>
+							<div class='determinate' style='width: <?php print round(100 * $totalDonationAmount / $amount); ?>%'></div>
 						</div>
-						<div class="valign-wrapper"><i class="material-icons small" style="padding:0 2% 0 0%">access_time</i>10 days left to contribute</div>
+						<div class="valign-wrapper"><i class="material-icons small" style="padding:0 2% 0 0%">access_time</i>
+						<?php $currentTime = time();
+						$timeDiff = $deadline - $currentTime;
+							if ($timeDiff < 0) {
+								print "This fundraiser has ended.";
+							} elseif ($timeDiff < 3600) {
+								print round($timeDiff / 60)." minutes left to contribute";
+							} elseif ($timeDiff < 3600 * 24) {
+								print round($timeDiff / 3600)." hours left to contribute";
+							} else {
+								print round($timeDiff / (24 * 3600))." days left to contribute";
+							} ?>
+ 							</div>
 <br>
 					
 		<div class="left-align">
 				
-				<a href=''
+				<a href='one_time_payment_view.php?fundraiserId=<?php print $id; ?>'
 				id="donate-button"
 				class="waves-effect waves-light donor-background lighten-1 btn-large" style="width:100%; border-radius:10px;font-size: large">Donate</a>
 			
@@ -149,8 +185,8 @@ if ($row = $result->fetch_assoc()) {
 
 		
 		<div class="center-align" style="padding:8% 2% 2% 2%;">
-		<img src="uploads/<?php print $similarPicture; ?>" class="responsive-img" style='border: black 2px solid; box-shadow: 10px 10px 5px #888888; border-radius:10px;'>	
-		<div style="font-weight:thin; font-size:large">here's a similar project</div>
+		<div class="responsive-img" style="background-image:url('uploads/<?php print $similarPicture; ?>');background-position:center;background-size:cover;border: black 2px solid; box-shadow: 10px 10px 5px #888888; border-radius:10px;height:400px;"></div>
+		<div style="font-weight:thin; font-size:large;margin-top:10px;">here's a similar project</div>
 		</div>
 		</div>
 		
@@ -160,16 +196,20 @@ if ($row = $result->fetch_assoc()) {
             	
             <h5 style="padding: 2% 0% 0% 0%; font-size:20px; font-weight:400;">Fundraising Timeline</h5>
             <div style="overflow-x:scroll; height:200px;width:100%;">
+            <?php 
+            $donationCount = count($donationAmounts);
+            for ($i = 0; $i < $donationCount; $i++) { ?>
             <div class="row valign-wrapper">
             <div style="padding 0 0 0 0%;margin: 3% 3% 3% 3%; display:inline-block;background-color: teal;border-radius:50%; border-color:black;border-width:thin; height:80px; width:80px;">
-                 				<a class="tooltip" style='text-decoration:none;color:#EEEEEE;'><span class="tooltiptext">Thanks, Jeff D!</span><span style="height:80px; width:80px; padding: 0 0% 0 0%; font-size: x-large; font-color: #ffffff; 
-                                    text-align: center;display: table-cell;vertical-align:middle;"><b>JD</b></span></a>
+                 				<span style="height:80px; width:80px; padding: 0 0% 0 0%; font-size: x-large; font-color: #ffffff; 
+                                        text-align: center;display: table-cell;vertical-align:middle;"><b><?php print $donationInitials[$i];?></b></span>
                  			
  			</div>
- 			<div style="padding:0 0 0% 5%;vertical-align:middle; display: inline-block;"><span style="font-size: 16px; font-weight: 300"><b>Tom and Patty Smith</b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> donated $50</span><br/><span style='text-align:right;'>3 hours ago</span>
+ 			<div style="padding:0 0 0% 5%;vertical-align:middle; display: inline-block;"><span style="font-size: 16px; font-weight: 300"><b><?php print $donationNames[$i]; ?></b></span><span style="font-size: medium; font-weight: 300; text-color:#efebe9"> donated $<?php print $donationAmounts[$i]; ?></span><br/><span style='text-align:right;'>on <?php print date('M j, Y', $donationDates[$i]); ?></span>
  			
- 			<div style="font-weight: 200; font-size:16px;">Happy birthday, Bryan! We love you.</div>
- 			</div>
+ 			<div style="font-weigh: 200; font-size:16px;"><?php print $donationMessages[$i]; ?></div>
+ 			</div>t
+ 			<?php } ?>
 					
 		</div>
 		
