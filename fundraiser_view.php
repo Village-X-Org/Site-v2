ceaccea6e80d1f9236213527c43772ef078a4517
Jeff Depree <jdepree@gmail.com>
2018-05-15 07:19:17 -0600

    Some data

--- fundraiser_view.php
+++ fundraiser_view.php
@@ -1,5 +1,40 @@
 <?php
 require_once("utilities.php");
+
+if (!hasParam('id')) {
+	print "No fundraiser id specified";
+	return;
+}
+$id = param('id');
+
+$stmt = prepare("SELECT fundraiser_title, fundraiser_subject, fundraiser_amount, fundraiser_description, fundraiser_deadline, project_name, village_name, country_label,
+		project_budget, vs1.stat_value AS peopleCount, vs2.stat_value AS houseCount, picture_filename
+		FROM fundraisers JOIN projects ON fundraiser_project_id=project_id 
+		JOIN villages ON project_village_id=village_id 
+		JOIN countries ON village_country=country_id
+		JOIN pictures ON project_similar_image_id=picture_id
+		LEFT JOIN village_stats AS vs1 ON vs1.stat_village_id=village_id AND vs1.stat_type_id=18 AND YEAR(project_date_posted)=vs1.stat_year
+        LEFT JOIN village_stats AS vs2 ON vs2.stat_village_id=village_id AND vs2.stat_type_id=19 AND YEAR(project_date_posted)=vs2.stat_year
+		WHERE fundraiser_id=?");
+$stmt->bind_param('i', $id);
+$result = execute($stmt);
+if ($row = $result->fetch_assoc()) {
+	$title = $row['fundraiser_title'];
+	$amount = $row['fundraiser_amount'];
+	$description = $row['fundraiser_description'];
+	$subject = $row['fundraiser_subject'];
+	$deadline = $row['fundraiser_deadline'];
+	$projectName = $row['project_name'];
+	$villageName = $row['village_name'];
+	$countryName = $row['country_label'];
+	$peopleCount = $row['peopleCount'];
+	$houseCount = $row['houseCount'];
+	$similarPicture = $row['picture_filename'];
+	$villageContribution = round($row['project_budget'] * .05);
+} else {
+	print "Fundraiser not found";
+	return;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,18 +49,18 @@ require_once("utilities.php");
 		<div class="row">
 		  <div class="col s6 m6 l2 right-align hide-on-med-and-down" style="padding: 9% 0% 0% 3%;">
 			<div style="width:200px; height:200px; border-radius:20%; border-style:solid; background:#008080CC;">
-        				<h1 class="header center-align light" style="padding:9% 0 0 0;font-size:96px;"><b>B</b></h1>
+        				<h1 class="header center-align light" style="padding:9% 0 0 0;font-size:96px;"><b><?php print $subject[0]; ?></b></h1>
 			</div>
 		</div> 
 	
 		<div class="col s6 m6 l6 left-align hide-on-med-and-down" style="padding: 5% 1% 1% 2%;">
 			<div style="padding: 5% 5% 5% 5%">
-				<h3 class="header col s12 white-text text-lighten-2">Bryan's 35th Birthday</h3>
+				<h3 class="header col s12 white-text text-lighten-2"><?php print $title; ?></h3>
 			</div>
 
 			<div style="padding: 5% 5% 5% 5%;">
 				<h5 class="header light" style="padding:0% 3% 0% 2%">
-					Bryan Nelson</h5>
+					<?php print $subject; ?></h5>
 			</div>
 
 			<div style="padding: 0% 5% 5% 7%;">
@@ -40,12 +75,12 @@ require_once("utilities.php");
 		
 			<div class="col s12 m12 l6 center-align hide-on-large-only" style="padding: 12% 1% 1% 1%;">
 			<div style="padding: 5% 5% 5% 5%">
-				<h3 class="header col s12 white-text text-lighten-2 text-shadow: 2px 2px 7px #111111">Bryan's 35th Birthday</h3>
+				<h3 class="header col s12 white-text text-lighten-2 text-shadow: 2px 2px 7px #111111"><?php print $title; ?></h3>
 			</div>
 
 			<div style="padding: 5% 5% 5% 5%;">
 				<h5 class="header light text-shadow: 2px 2px 7px #111111" style="padding:0% 3% 0% 3%">
-					Bryan Nelson</h5>
+					<?php print $subject; ?></h5>
 			</div>
 
 			<div style="padding: 0% 5% 5% 5%;">
@@ -56,8 +91,8 @@ require_once("utilities.php");
 			</div>
 			
 			<div style="padding: 5% 20% 5% 20%;">
-				<h6 class="header light text-shadow: 2px 2px 7px #111111" style="font-weight:thin">fundraising to <span style="font-size: large; font-weight: bold">Provide Clean Water</span></h6>
-				<h6>in Saiti Village, Malawi</h6>
+				<h6 class="header light text-shadow: 2px 2px 7px #111111" style="font-weight:thin">fundraising to <span style="font-size: large; font-weight: bold"><?php print $projectName; ?></span></h6>
+				<h6>in <?php print $villageName; ?> Village, <?php print $countryName; ?></h6>
 			</div>
 			
 			</div>
@@ -71,8 +106,8 @@ require_once("utilities.php");
         </div> -->
         <div class="card-content">
 	        <h5 class="header white-text text-lighten-2">fundraising to</h5>
-	          <p style='font-size:28px;'><b>Provide Clean Water</b><br/><span style='margin-top:10px;text-size:24px;'> in Saiti Village, Malawi</span></p>
-	          <p style="margin-top:10px;margin-bottom:20px;">Helping 1420 people across 178 households. Saiti has contributed $310, materials, and labor.</p>
+	          <p style='font-size:28px;'><b><?php print $projectName; ?></b><br/><span style='margin-top:10px;text-size:24px;'> in <?php print $villageName; ?> Village, <?php print $countryName; ?></span></p>
+	          <p style="margin-top:10px;margin-bottom:20px;">Helping <?php print $peopleCount; ?> people across <?php print $houseCount; ?> households. <?php print $villageName; ?> has contributed $<?php print $villageContribution; ?>, materials, and labor.</p>
 	        <div class="card-action center-align">
 	          <a href="#">meet the village</a>
 	        </div>
@@ -114,16 +149,14 @@ require_once("utilities.php");
 
 		
 		<div class="center-align" style="padding:8% 2% 2% 2%;">
-		<img src="images/woman_with_goat" class="responsive-img" style='border: black 2px solid; box-shadow: 10px 10px 5px #888888; border-radius:10px;'>	
+		<img src="uploads/<?php print $similarPicture; ?>" class="responsive-img" style='border: black 2px solid; box-shadow: 10px 10px 5px #888888; border-radius:10px;'>	
 		<div style="font-weight:thin; font-size:large">here's a similar project</div>
 		</div>
 		</div>
 		
 		<div class="col s12 m12 l6 left-align" style="vertical-align: middle;padding: 0% 2% 2% 3%">
 						
-      <div class="flow-text" style="padding: 3% 0% 0% 0%; font-size:22px;">Many of us have no idea what it's like to be thirsty. We have plenty of water to drink -- even the water in our toilets is clean! But many people around the 
-      world don’t have that luxury. Every day, about 1,400 children die from diseases caused by unsafe water and poor sanitation. But it doesn’t have to be that way. There are simple solutions like drilled wells, spring 
-      protections and BioSand filters that help provide clean water to communities around the world.</div>
+      <div class="flow-text" style="padding: 3% 0% 0% 0%; font-size:22px;"><?php print $description;?></div>
             	
             <h5 style="padding: 2% 0% 0% 0%; font-size:20px; font-weight:400;">Fundraising Timeline</h5>
             <div style="overflow-x:scroll; height:200px;width:100%;">
