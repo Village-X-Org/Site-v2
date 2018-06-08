b4495a2b4433227c06c521d5068864b007053866
Jeff Depree <jdepree@gmail.com>
2018-06-08 16:14:58 -0600

    Reset and z-index breaking browsers

--- user_login.php
+++ user_login.php
@@ -21,7 +21,6 @@ body, html {
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
-    z-index: -1;
   -webkit-transform: translate3d(0, 0, 0);
           transform: translate3d(0, 0, 0);
     
@@ -59,8 +58,7 @@ include('header.inc');
                         <div class="errorTxt3 center-align" style="font-size:10px; color:red;">
                         </div>
                       </div>
-                    </div>
-                  </row>
+                  </div>
                             
                   <div class="row" style="padding:2% 3% 0 3%;margin:0;">
                     <div class="black-text" style="font-size:large; padding:0 0 0 3%"><b>PASSWORD</b>
@@ -71,23 +69,30 @@ include('header.inc');
                       </div>
                     </div>
                   </div>
- 
-              </div>
-                  
-              <div class="center-align valign-wrapper" style="width:100%; padding:0 3% 0% 3%;">
-                 <div class="input-field center-align" style="width:100%;">
-                         
-                      <button id="loginButton" style="width:100%;height:70px;font-size:25px"
-                          class="g-recaptcha btn-large donor-background center-align submit"
-                          data-sitekey="<?php print CAPTCHA_SITEKEY; ?>"
-                          data-callback="onSubmit">
-                          SIGN IN 
-                      </button>
-                    <div id='loginErrorText' style='margin-top:10px;color:red;'></div>
-                 </div>    
-             </div>
+                    
+                  <div class="center-align valign-wrapper" style="width:100%; padding:0 3% 0% 3%;">
+                     <div class="input-field center-align" style="width:100%;">
+                             
+                          <button id="loginButton" style="width:100%;height:70px;font-size:25px"
+                              class="g-recaptcha btn-large donor-background center-align submit"
+                              data-sitekey="<?php print CAPTCHA_SITEKEY; ?>"
+                              data-callback="onSubmit">
+                              SIGN IN 
+                          </button>
+                        <div id='loginErrorText' style='margin-top:10px;color:red;'></div>
+                     </div>    
+                  </div>
+                </div>
+            </form>
           </div>
-        </form>
+          <div class="black-text container center-align" style="font-weight:400;font-size:x-large;padding:0% 0 0% 0">
+            Don't have an account? <a href="user_register.php"><span class="blue-text" style='font-weight:bold;'>Sign up</span></a>
+          </div>
+                               
+          <div class="black-text center-align" style="padding:1% 0 3% 0; width:100%;font-weight:400;font-size:x-large;">
+            Forgot your password? <a class='modal-trigger blue-text' style='font-weight:bold;' href="#passwordModal">Reset it</a>
+          </div>              
+        </div> 
         <script>
             function onSubmit(token) {
                 $('#login_form').submit();
@@ -129,18 +134,9 @@ include('header.inc');
                     });
               });
         </script>
-             
-        <div class="black-text container center-align" style="font-weight:400;font-size:x-large;padding:2% 0 0% 0">
-          Don't have an account? <a href="user_register.php"><span class="blue-text" style='font-weight:bold;'>Sign up</span></a>
-        </div>
-                               
-        <div class="black-text center-align" style="padding:1% 0 1% 0; width:100%;font-weight:400;font-size:x-large;">
-          Forgot your password? <a class='modal-trigger blue-text' style='font-weight:bold;' href="#passwordModal">Reset it</a>
-        </div>              
-      </div> 
+            
     </div>
-  </div>               
-</div>      
+  </div>     
 
   <div id="passwordModal" class="modal">
     <div class="modal-content">
@@ -150,11 +146,11 @@ include('header.inc');
         </div>
         <div class="container center-align"
             style="padding: 0% 5% 0% 5%;">
-            <form id="resetPassword_form" method="get" class="center-align">
+            <form id="reset_password_form" method="get" class="center-align">
                 <div class="row center-align">
                     <div class="col s12 m12 l12 center-align">
                         <div class="input-field col s12 center-align">
-                            <input placeholder="jane@gmail.com" id="resetPW_email"
+                            <input placeholder="jane@gmail.com" id="reset_password_email"
                                 name="email" required data-error=".errorTxt3"
                                 style="font-size: 20px; color: black;" type="email">
                             <div class="errorTxt3"
@@ -167,14 +163,60 @@ include('header.inc');
                 <div class="row s12 m12 l12 center-align"
                     style="padding: 0% 0% 0% 0%;">
                     <div class="input-field">
-                        <button id="submitBtn" class="btn-large red submit" type="submit"
-                            name="action" style="width: 100%;">Reset My Password</button>
+                      <button id="resetButton" style="width:100%;height:70px;font-size:25px"
+                          class="g-recaptcha btn-large red center-align submit"
+                          data-sitekey="<?php print CAPTCHA_SITEKEY; ?>"
+                          data-callback="onReset">
+                          Reset My Password
+                      </button>
                     </div>
                     <div class="progress" style='display: none;' id='cancelProgress'>
                         <div class="indeterminate"></div>
                     </div>
                 </div>
             </form>
+            <script>
+            function onReset(token) {
+              $("#reset_password_form").submit();
+            }
+            $().ready(function() {
+              $('.modal').modal();
+
+              $("#reset_password_form").validate({
+                rules: {
+                  email: "required",
+                  password: "required",
+                  
+                },
+              messages: {
+                    email: "a valid email address is required",
+                    password: "a valid password is required",
+              },
+              errorElement : 'div',
+                errorPlacement: function(error, element) {
+                    var placement = $(element).data('error');
+                    if (placement) {
+                      $(placement).append(error)
+                    } else {
+                      error.insertAfter(element);
+                    }
+                    grecaptcha.reset();
+                },
+                  submitHandler: function(form) {
+                    $.post( "user_resetPassword.php", $( "#reset_password_form" ).serialize())
+                        .done(function( data ) {
+                          if (data == 'success') {
+                            $('#passwordModal').modal('close');
+                            alert("An email with instructions has been mailed to the address you provided!");
+                          } else {
+                            alert("We could not find the supplied email in our system.");
+                          }
+                          grecaptcha.reset();
+                    });
+                  } 
+              });
+            });
+          </script>
         </div>     
     </div>
   </div>      
