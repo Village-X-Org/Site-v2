8825ed9b192920a3d6600f0d1cad67d57baa5ae4
Jeff Depree <jdepree@gmail.com>
2018-06-08 17:29:40 -0600

    New password reset screen

---
+++ user_changePassword.php
@@ -0,0 +1,24 @@
+<?php
+require_once("utilities.php");
+
+$id = param('id');
+$code = param('code');
+$newPassword = md5(param('newPassword'));
+
+$stmt = prepare("UPDATE users SET user_password=? WHERE donor_id=? AND md5(concat(donor_email, donor_password))=?");
+$stmt->bind_param('sis', $newPassword, $id, $code);
+execute($stmt);
+if (mysql_affected_rows() > 0) {
+	$stmt = prepare("SELECT donor_id, donor_first_name, donor_last_name, donor_email FROM donors WHERE donor_id=?");
+	$stmt->bind_param('i', $id);
+	$result = execute($stmt);
+	if ($row = $result->fetch_assoc()) {
+		$session_donor_id = $_SESSION['donor_id'] = $row['donor_id'];
+		$session_first_name = $_SESSION['first_name'] = $row['donor_first_name'];
+		$session_last_name = $_SESSION['last_name'] = $row['donor_last_name'];
+		$session_email = $_SESSION['email'] = $row['donor_email'];
+		include('user_profile.php');
+	}
+} else {
+	print "Bad code";
+}
