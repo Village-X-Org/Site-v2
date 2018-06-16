db7b01f5334c7abd341d3b1d299467269b98f3dc
Jeff DePree <jdepree@gmail.com>
2018-06-16 06:52:33 -0600

    Login background

--- user_login.php
+++ user_login.php
@@ -30,9 +30,9 @@ body, html {
 include('header.inc');
 ?>
 
-<div class="bg valign-wrapper" style='height:100%;width:100%'>
+<div class="bg" style='height:100%;width:100%;min-height:800px;'>
 
-  <div class="container" style="width:100%;padding:10% 10% 0 10%">
+  <div class="container" style="width:100%;padding:3% 10% 0 10%">
 
     <div class="white-text center-align" style="font-weight:800;font-size:xx-large;text-shadow: black 0.1em 0.1em 0.4em;">WELCOME BACK
     </div>
