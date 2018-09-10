11735b88c8876c13d5898ebb5199cd9b23321342
Jeff Depree <jdepree@gmail.com>
2018-09-10 10:29:46 -0600

    Clickable pictures and better descriptions

--- updates.php
+++ updates.php
@@ -1,7 +1,11 @@
 <?php
 require_once("utilities.php");
+$projectId = 0;
+if (hasParam('projectId')) {
+	$projectId = paramInt('projectId');
+}
 $result = doUnprotectedQuery("SELECT ru_date, ru_description, ru_picture_ids, project_name, project_id FROM raw_updates 
-	JOIN projects ON ru_project_id=project_id ORDER BY ru_project_id, ru_date DESC");
+	JOIN projects ON ru_project_id=project_id ".($projectId ? "AND project_id=$projectId " : "")."ORDER BY ru_project_id, ru_date DESC");
 $lastProjectId = 0;
 while ($row = $result->fetch_assoc()) {
 	$projectId = $row['project_id'];
@@ -18,13 +22,13 @@ while ($row = $result->fetch_assoc()) {
 	}
 	$lastProjectId = $projectId;
 
-	print "<H4>$date - $description</H4>";
+	print "<p><b>$date</b> ".($description ? " -- ".$description." -- " : "")."</p>";
 
 	if (strlen($pictureIds) > 0) {
 		$resultPics = doUnprotectedQuery("SELECT picture_filename FROM pictures WHERE picture_id IN ($pictureIds)");
 		while ($rowPics = $resultPics->fetch_assoc()) {
 			$picFilename = $rowPics['picture_filename'];
-			print "<img src='uploads/$picFilename' style='width:300px;' />";
+			print "<a href='uploads/$picFilename' target='_blank' style='border:0;'><img src='uploads/$picFilename' style='width:300px;' /></a>";
 		}
 		$resultPics->close();
 	}
