ebb1dfe373b5089475077397382fc20c8a48f5fd
malawi-mike <mike@villagex.org>
2019-03-30 12:16:27 -0400

    added finances page and edits model page

---
+++ finances.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('header.inc'); ?>
+<div id="index-banner" class="parallax-container valign-wrapper" style="background-color: rgba(0, 0, 0, 0.3); height: 500px;">
+
+    <div class="section no-pad-bot valign-wrapper" style="height: 100%; width:100%;">
+        	<div class="row center">
+        		<h2 class="header col s12 white-text text-lighten-2 text-shadow: 2px 2px 7px #111111">Finances</h2>
+        
+                	<div class="parallax" style="background-size: cover;">
+                		<img src="images/malawi_money.jpg">
+                	</div>
+        	</div>
+    </div>
+</div>
+
+<div class="container">
+
+
+<div class="section text-lighten-2 text-shadow: 2px 2px 7px #111111">
+	<h4>
+			Use of Donations
+	</h4>
+	
+	<div class="section flow-text" style="padding:1% 3% 2% 3%;">A direct donation experience means tracking every dollar. Each project profile features a cost breakdown showing how your donations distrupt extreme poverty. 
+						Each breakdown shows that we collect 10% of every donation to acheive <b>sustainability</b>. We use this money to improve our technology 
+						and cover adminstrative costs (e.g., licensing fees) we incur as a lean startup. None of our US-based staff receives a salary. 
+                    		As shown below, villages receive 80% or more of each donation for direct project costs like building materials. Credit card and money transfer fees account for about 10% (ironically, it takes money to move money).*</div>
+             
+<div class="row" style="padding:3% 5% 0% 3%;">
+		<div class="col s12 m4 l4">
+			<div class="icon-block center" style="padding:0% 0% 6% 0%;">
+				<i class="material-icons" style="font-size: 30px">build</i>
+			<h5>direct project costs</h5>
+			<h6 class="light center">labor, materials, field officers</h6>
+			<h4 class="light center">~80%</h4>
+</div>
+</div>
+
+		<div class="col s12 m4 l4">
+			<div class="icon-block center" style="padding:0% 9% 6% 9%;">
+			<i class="material-icons" style="font-size: 30px">account_balance</i>
+			<h5>bank fees*</h5>
+			<h6 class="light center">credit card and money transfer fees</h6>
+			<h4 class="light center">~10%</h4>
+
+</div>
+</div>
+
+		<div class="col s12 m4 l4" style="padding:0% 0% 3% 0%;">
+			<div class="icon-block center">
+			<i class="material-icons" style="font-size: 30px">all_inclusive</i>
+			<h5>admin</h5>	
+			<h6 class="light center">tech updates, insurance, licensing</h6>
+			<h4 class="light center">10%</h4>
+</div>
+</div>
+</div>
+<h6 class="center-align" style="padding:0% 3% 2% 3%;">*if 10% sounds high, remember that money transfer fees include explicit fees and unfavorable exchange rate conversions</h6>	
+
+             
+    <div class="section flow-text" style="padding:1% 3% 0% 3%;">Our transparent approach is a vast improvement over the industry standard. Many NGOs report that 85% or 90% of funds go to “programmatic expenses,” 
+    						an umbrella term for any cost associated with a program or intiative (e.g., SUVs). However, anyone operating in the field knows that only
+                    		a fraction of that money reaches the ground for direct project costs like cement (exact percentages are unknown because NGOs often don’t 
+                    		make their budgets public). In their seminal book, <a href="https://books.google.com/books?id=yIV_NMDDIvYC&pg=PA452&lpg=PA452&dq=%22studies+estimate+that+only+about+10+or+at+most+20+percent+of+aid+ever+reaches+its+target%22&source=bl&ots=_0Ss4wsrJv&sig=ACfU3U3V0UKC4ClKLll0TnMe9K3rbGoFbg&hl=en&sa=X&ved=2ahUKEwiokJSJhKjhAhWNT98KHYEXBjsQ6AEwAHoECAkQAQ#v=onepage&q=%22studies%20estimate%20that%20only%20about%2010%20or%20at%20most%2020%20percent%20of%20aid%20ever%20reaches%20its%20target%22&f=false" target="_blank">
+                    		Why Nations Fail</a>, Professors Daron Acemoglu and James A Robinson write that 
+                    		"[m]any studies estimate that only about 10 or at most 20 percent of aid ever reaches its target." Village X changes that by transfering cash directly from donor to recipient village.
+     </div>
+	
+	<div class="row hide-on-small-only" style="padding:5% 5% 0% 3%;">
+		<div class="col s12 m4 l4">
+			<div class="icon-block center" style="padding:0% 0% 3% 0%;">
+				<i class="material-icons" style="font-size: 50px">directions_walk</i>
+			<h5>donors</h5>
+			<h6 class="light center">cash, questions, encouragement</h6>
+			
+</div>
+</div>
+
+		<div class="col s12 m4 l4">
+			<div class="icon-block center" style="padding:0% 9% 3% 9%;">
+			<i class="material-icons" style="font-size: 50px">swap_horizontal_circle</i>
+			<h5>direct connection</h5>
+			<h6 class="light center">two-way information flow</h6>
+			
+
+</div>
+</div>
+
+		<div class="col s12 m4 l4" style="padding:0% 0% 3% 0%;">
+			<div class="icon-block center">
+			<i class="material-icons" style="font-size: 50px;transform: rotateY(180deg);">directions_walk</i>
+			<h5>villages</h5>	
+			<h6 class="light center">projects, pics/videos, impact data</h6>
+			
+</div>
+</div>
+</div>
+
+</div>
+
+<hr>
+<div class="left text-lighten-2 text-shadow: 2px 2px 7px #111111" style="width:100%; padding: 1% 0 1% 0">
+<h4>
+			2018 Financial Report
+	</h4>
+</div>
+
+<div class="section flow-text" style="padding:0% 3% 4% 3%;"> 
+Below is a table showing exactly how we spent your hard-earned money in 2018.  
+This is part of our 100% radically transparent commitment to prove (not just say) that we are good stewards of your donations.</div>
+	
+<div style="padding: 0 0 3% 0">
+<object data="finances_2018.pdf" type="application/pdf" width="100%" height="800px"> 
+  <p>It appears you don't have a PDF plugin for this browser.
+   No biggie... you can <a href="finances_2018.pdf">click here to
+  download the PDF file.</a></p>  
+</object>
+</div>
+
+</div>
+
+<?php include('footer.inc'); ?>
