244e4d9f03856956f4031277b3080d553ffc8c4d
malawi-mike <mike@villagex.org>
2019-07-10 22:14:55 -0400

    removed text-shadow tags

--- village_add_thanks.php
+++ village_add_thanks.php
@@ -33,17 +33,17 @@ include('header.inc'); ?>
 	<div class="valign-wrapper">
 		<div>
         		<div class="row left-align">
-        			<h3 class="header col s12 black-text text-lighten-2 text-shadow: 2px 2px 7px #111111 flow-text" style="font-weight: 300">Thanks for adding <?php print $villageName; ?>!</h3>
-        			<h4 class="header col s12 black-text text-lighten-2 text-shadow: 2px 2px 7px #111111 flow-text" style="font-weight: 300">We put your village on our map to recognize your commitment to community-led 
+        			<h3 class="header col s12 black-text text-lighten-2 flow-text" style="font-weight: 300">Thanks for adding <?php print $villageName; ?>!</h3>
+        			<h4 class="header col s12 black-text text-lighten-2 flow-text" style="font-weight: 300">We put your village on our map to recognize your commitment to community-led 
         			development and share your village's development needs with organizations that might be able to help.</h4> 
-        			<h4 class="header col s12 black-text text-lighten-2 text-shadow: 2px 2px 7px #111111 flow-text" style="font-weight: 300">Please add more villages to the map and encourage your friends to do the same.</h4>
-        			<h4 class="header col s12 black-text text-lighten-2 text-shadow: 2px 2px 7px #111111 flow-text" style="font-weight: 300">Sincerely,</h4>
-        			<h4 class="header col s12 black-text text-lighten-2 text-shadow: 2px 2px 7px #111111 flow-text" style="font-weight: 300">Village X Team</h4>
+        			<h4 class="header col s12 black-text text-lighten-2 flow-text" style="font-weight: 300">Please add more villages to the map and encourage your friends to do the same.</h4>
+        			<h4 class="header col s12 black-text text-lighten-2 flow-text" style="font-weight: 300">Sincerely,</h4>
+        			<h4 class="header col s12 black-text text-lighten-2 flow-text" style="font-weight: 300">Village X Team</h4>
         		 
         		</div>
 			<br>
 			<div class="row center">
-				<h5 class="black-text text-lighten-2 text-shadow: 2px 2px 7px #111111 flow-text"><b>Share the good news on Facebook and Twitter!</b>
+				<h5 class="black-text text-lighten-2 flow-text"><b>Share the good news on Facebook and Twitter!</b>
 				</h5>	
 			</div> 
 			<div class="row center">
