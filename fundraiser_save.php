f3bdc9b328f1068905e2ecb8206d49559075e494
Jeff DePree <jdepree@gmail.com>
2020-01-20 20:15:27 -0800

    Ignore zero-amount fundraisers

--- fundraiser_save.php
+++ fundraiser_save.php
@@ -9,7 +9,7 @@ $amount = paramInt('fundraiser_amount');
 $description = param('fundraiser_description');
 $captcha = param('g-recaptcha-response');
 
-if (!verifyRecaptcha($captcha)) {
+if (!verifyRecaptcha($captcha) || $amount == 0) {
 	print "Google has decided you are a robot.  If you think this is an error, please tell the site administrator, or maybe just try again.";
     emailAdmin("Robot detected in fundraiser_create", "Someone tried to create a fundraiser with donor id: $donorId, Title: $title, Description: $description, Amount: $amount, and email: ".(hasParam('fundraiser_email') ? param('fundraiser_email') : ""));
     die(1);
