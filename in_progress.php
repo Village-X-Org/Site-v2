68ecdddac2906847a7bec17120902daca90ed37a
Jeff DePree <jdepree@gmail.com>
2020-02-26 13:40:59 -0800

    Hide cancelled projects

--- in_progress.php
+++ in_progress.php
@@ -2,8 +2,8 @@
 require_once("utilities.php");
 
 $result = doUnprotectedQuery("SELECT project_id, project_name, village_name, MAX(pe_date) AS maxDate, MAX(pe_type) AS maxType, fo_first_name, fo_last_name 
-	FROM projects JOIN villages ON project_village_id=village_id 
-	JOIN project_events ON pe_project_id=project_id JOIN field_officers ON  project_staff_id=fo_id GROUP BY project_id HAVING maxType=3 ORDER BY maxDate ASC");
+	FROM projects JOIN villages ON project_status<>'cancelled' AND project_village_id=village_id 
+	JOIN project_events ON pe_project_id=project_id JOIN field_officers ON project_staff_id=fo_id GROUP BY project_id HAVING maxType=3 ORDER BY maxDate ASC");
 
 while ($row = $result->fetch_assoc()) {
 	$date = strtotime($row['maxDate']);
