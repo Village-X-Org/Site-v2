cfa8d81f091514db3827b0515cacf8a2e6614b4a
Jeff DePree <jdepree@gmail.com>
2020-12-05 17:29:31 -0800

    Donations Since changes

--- utility_donationsSince.php
+++ utility_donationsSince.php
@@ -1,27 +1,27 @@
 <?php
 require_once("utilities.php");
-$since = paramInt('since');
-$user = param('u');
-$password = md5(param('p'));
-
-$stmt = prepare("SELECT user_id FROM users WHERE user_username=? AND user_password=?");
-$stmt->bind_param('ss', $user, $password);
-$result = execute($stmt);
-if (!($row = $result->fetch_assoc())) {
-    die(1);
+if (!hasParam('since')) {
+	$since = 0;
+} else {
+	$since = paramInt('since');
 }
+if (isset($session_is_admin) && $session_is_admin) {
 
-$stmt = prepare("SELECT donation_id, donor_email, donor_first_name, donor_last_name, 
-        donation_amount, donation_date, donation_project_id, donation_subscription_id FROM donations 
-        JOIN donors ON donation_donor_id=donor_id WHERE donation_id>? ORDER BY donation_date DESC");
-$stmt->bind_param('i', $since);
-$result = execute($stmt);
-print "<pre>";
-while ($row = $result->fetch_assoc()) {
-    print $row['donation_id'].",".$row['donor_email'].",".$row['donor_first_name'].",".$row['donor_last_name'].",".$row['donation_amount']
-            .",".$row['donation_date'].",".$row['donation_project_id'].",".$row['donation_subscription_id']."\n";
+	$stmt = prepare("SELECT donation_id, donor_email, donor_first_name, donor_last_name, 
+	        donation_amount, donation_date, donation_project_id, donation_subscription_id, donation_matched_to FROM donations 
+	        JOIN donors ON donation_donor_id=donor_id WHERE donation_id>? ORDER BY donation_date DESC");
+	$stmt->bind_param('i', $since);
+	$result = execute($stmt);
+	print "<pre>";
+	while ($row = $result->fetch_assoc()) {
+	    print $row['donation_id'].",".$row['donor_email'].",".$row['donor_first_name'].",".$row['donor_last_name'].",".$row['donation_amount']
+	            .",".$row['donation_date'].",".$row['donation_project_id'].",".$row['donation_subscription_id'].",".$row['donation_matched_to']."\n";
+	}
+	print "</pre>";
+	$stmt->close();
+
+} else {
+	print "Login with your admin account first";
 }
-print "</pre>";
-$stmt->close();
 
 ?>
